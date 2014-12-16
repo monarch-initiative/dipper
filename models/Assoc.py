318ed60d18a57be6bf2efc3179001bef053c076f
Nicole Washington <nicole.l.washington@gmail.com>
2014-12-16 11:21:07 -0800

    added relations, fixed some uris

--- models/Assoc.py
+++ models/Assoc.py
@@ -1,25 +1,47 @@
 __author__ = 'nicole'
 
-from rdflib import Namespace
+from rdflib import Namespace, OWL, RDF, URIRef
 
 
 class Assoc:
     ###### Namespaces ######
     # TODO turn this into a dictionary from the context file
-    namespaces = {
-        'MONARCH': 'http://www.monarchinitiative.org/',
-        'HP': 'http://purl.obofoundry.org/obo/HP_',
-        'ECO': 'http://purl.obofoundry.org/obo/ECO_',
-        'PMID': 'http://www.ncbi.nlm.nih.gov/pubmed/'
+    curie_map = {
+        'MONARCH': 'http://www.monarchinitiative.org/MONARCH_',
+        'HP': 'http://purl.obolibrary.org/obo/HP_',
+        'ECO': 'http://purl.obolibrary.org/obo/ECO_',
+        'PMID': 'http://www.ncbi.nlm.nih.gov/pubmed/PMID_',
+        'RO' : 'http://purl.obolibrary.org/obo/RO_',
+        'OBO' : 'http://purl.obolibrary.org/obo/'
     }
+
+    relationships = {
+        'has_disposition':'http://purl.obolibrary.org/obo/GENO_0000208',
+        'has_phenotype':'http://purl.obolibrary.org/obo/RO_0002200'
+    }
+
+
+    OWLCLASS=OWL['Class']
+    OWLIND=OWL['NamedIndividual']
+    OWLANNOT=OWL['Annotation']
+    OWLPROP=OWL['ObjectProperty']
+
 
     def __init__(self):
         return
 
     def get_namespaces(self):
-        return self.namespaces
+        return self.curie_map
+
+    def get_relationships(self):
+        return self.relationships
 
     def createAssociationNode(self, g):
         #todo make a general association object following our pattern
 
+        return
+
+    def loadObjectProperties(self,g):
+        for k in self.relationships:
+            g.add((URIRef(self.relationships[k]),RDF['type'],self.OWLPROP))
         return
