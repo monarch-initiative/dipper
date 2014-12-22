c863aa9865b5e8bcf65522b054a6f1d5e861d806
Nicole Washington <nicole.l.washington@gmail.com>
2014-12-22 13:39:03 -0800

    added omim with minimal output of classes.

--- __init__.py
+++ __init__.py
@@ -2,26 +2,32 @@
 #this will eventually control the processing of data sources
 __author__ = 'nlw'
 
+import config
 from sources.HPOAnnotations import HPOAnnotations
 from sources.ZFIN import ZFIN
+from sources.OMIM import OMIM
+
 
 source_to_class_map={
 #    'hpoa' : HPOAnnotations,
-    'zfin' : ZFIN
+#    'zfin' : ZFIN,
+    'omim' : OMIM
 }
+
+#load configuration parameters
+#for example, keys
+
 
 #TODO subset of sources will eventually be configurable on the commandline
 #iterate through all the sources
 for source in source_to_class_map.keys():
     mysource = source_to_class_map[source]()
     mysource.fetch()
-    mysource.parse(250)
+    mysource.parse(100)
     status = mysource.verify()
-#    if status is not True:
-#        print('ERROR: Source',source,'did not pass verification tests.')
+    if status is not True:
+        print('ERROR: Source',source,'did not pass verification tests.')
     print('***** Finished with',source,'*****')
-#    mysource.write("out/hpo_raw.rdf")
-#    mysource.write()
 print("All done.")
 
 #TODO command-line args:
--- sources/OMIM.py
+++ sources/OMIM.py
@@ -1,50 +1,50 @@
-import csv, os, datetime
-from datetime import datetime
+import os
 from stat import *
 import urllib
+from urllib import request
+import re
+import time
+from datetime import datetime
 import gzip,os.path
-from utils import fileutils
-
+import json
+from rdflib import Graph, Literal, URIRef, Namespace
+from rdflib.namespace import RDF, RDFS, OWL, DC
 from sources.Source import Source
 
 from models.D2PAssoc import D2PAssoc
 from models.DispositionAssoc import DispositionAssoc
-from rdflib import Namespace
 from models.Dataset import Dataset
 from models.Assoc import Assoc
+from utils.CurieUtil import CurieUtil
+from utils.GraphUtils import GraphUtils
+import config
 
-
-'''
-#down the entry files
-cat $DATAFILE | while read fileline ; do
-  log $fileline
-  # delete file before create it again
-  rm -f entry_$fileline.xml
-  sleep 0.5
-  wget -O entry_$fileline.xml "http://api.omim.org:8000/api/entry?include=all&apiKey=$apiKey&mimNumber=$fileline"
-done
-
-'''
 class OMIM(Source):
+    '''
+     OMIM is an unusual source.  We can get lots of the disease-gene associations, including allelic variants
+     from their ftp site, which is obtainable anonymously.  However, more detailed information is available
+     via their API.  So, we pull the basic files from their ftp site, extract the omim identifiers,
+     then query their API in batch.  (Note this requires an apiKey, which is not stored in the repo,
+     but in a separate conf.json file.)
+    '''
 
     files = {
         'all' : {
             'file' : 'omim.txt.gz',
             'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/omim.txt.Z'
         },
-        'genemap' : {
-            'file': 'genemap.txt',
-            'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/genemap'
-        },
-        'genemapkey' : {
-            'file': 'genemapkey.txt',
-            'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/genemap.key'
-
-        },
-        'morbidmap' : {
-            'file': 'morbidmap.txt',
-            'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/morbidmap'
-        }
+        #'genemap' : {
+        #    'file': 'genemap.txt',
+        #    'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/genemap'
+        #},
+        #'genemapkey' : {
+        #    'file': 'genemapkey.txt',
+        #    'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/genemap.key'
+        #},
+        #'morbidmap' : {
+        #   'file': 'morbidmap.txt',
+        #    'url' : 'ftp://anonymous:info%40monarchinitiative.org@ftp.omim.org/OMIM/morbidmap'
+        #}
     }
 
     disease_prefixes = {
@@ -52,6 +52,8 @@ class OMIM(Source):
     }
 
     curie_map = {}
+
+    OMIM_API = "http://api.omim.org:8000/api"
 
     def __init__(self):
         Source.__init__(self, 'omim')
@@ -67,9 +69,13 @@ class OMIM(Source):
         #data-source specific warnings (will be removed when issues are cleared)
         #print()
 
+        #check if config exists; if it doesn't, error out and let user know
+        if (not (('keys' in config.get_config()) and ('omim' in config.get_config()['keys']))):
+            print("ERROR: not configured with API key.")
         return
 
     def fetch(self):
+        #this is fetching the standard files, not from the API/REST service
         for f in self.files.keys():
             file = self.files.get(f)
             self.fetch_from_url(file['url'],('/').join((self.rawdir,file['file'])))
@@ -104,11 +110,131 @@ class OMIM(Source):
 
         print("Parsing files...")
 
-        return
+        omimids = []  #to store the set of omim identifiers
+        omimids = self._get_omim_ids()
 
+        omimparams = {
+            'format' : 'json',
+            'include' : 'all',
+        }
+        #you will need to add the API key into the conf.json file, like:
+        # keys : { 'omim' : '<your api key here>' }
+        omimparams.update({'apiKey' : config.get_config()['keys']['omim']})
+
+        #http://api.omim.org/api/entry?mimNumber=100100&include=all
+
+        g = self.graph
+
+        gu = GraphUtils(self.curie_map)
+        cu = CurieUtil(self.curie_map)
+
+        it=0  #for counting
+
+        #note that you can only do request batches of 20
+        #see info about "Limits" at http://omim.org/help/api
+        groupsize=20
+        if (limit is not None):
+            #just in case the limit is larger than the number of records, max it out
+            max = min((limit,omimids.__len__()))
+
+        #max = 10 #for testing
+
+        #TODO write the json to local files - make the assumption that downloads within 24 hrs are the same
+        #now, loop through the omim numbers and pull the records as json docs
+        while it < max:
+            end=min((max,it+groupsize))
+            #iterate through the omim ids list, and fetch from the OMIM api in batches of 20
+            omimparams.update({'mimNumber' : (',').join(omimids[it:end])})
+            p = urllib.parse.urlencode(omimparams)
+            url = ('/').join((self.OMIM_API,'entry'))+'?%s' % p
+            print ('fetching:',(',').join(omimids[it:end]))
+            #print ('fetching:',('/').join((self.OMIM_API,'entry'))+'?%s' % p)
+            it+=groupsize
+
+            d = urllib.request.urlopen(url)
+            resp = d.read().decode()
+            request_time = datetime.now()
+
+            myjson = json.loads(resp)
+            entries = myjson['omim']['entryList']
+
+            for e in entries:
+
+                #get the numbers, labels, and descriptions
+                omimnum = e['entry']['mimNumber']
+                titles = e['entry']['titles']
+                label = titles['preferredTitle']
+                description = self._get_description(e['entry'])
+
+                if (e['entry']['status'] == 'removed'):
+                    n = URIRef(cu.get_uri('OMIM:'+str(omimnum)))
+                    #make this a deprecated class
+                    g.add((n,RDF['type'],OWL['DeprecatedClass']))
+                else:
+                    gu.addClassToGraph(g,'OMIM:'+str(omimnum),label,None,description)
+
+                    #check if moved, if so, make it an equivalent class to the other thing
+                    if (e['entry']['status'] == 'moved'):
+                        gu.addEquivalentClass(g,'OMIM:'+str(omimnum),'OMIM:'+str(e['entry']['movedTo']))
+
+
+            #can't have more than 4 req per sec,
+            #so wait the remaining time, if necessary
+            dt=datetime.now()-request_time
+            rem=0.25-dt.total_seconds()
+            if (rem > 0):
+                print("INFO: waiting",str(rem),'s')
+                time.sleep(rem/1000)
+
+        ##### Write it out #####
+        filewriter = open(self.outfile, 'w')
+        self.load_core_bindings()
+        self.load_bindings()
+
+        print("Finished parsing files. Writing turtle to",self.outfile)
+        print(g.serialize(format="turtle").decode(),file=filewriter)
+        filewriter.close()
+
+
+        return
 
     def verify(self):
         status = True
         #TODO verify some kind of relationship that should be in the file
 
         return status
+
+    def _get_description(self,entry):
+        d = None
+        if entry is not None:
+            #print(entry)
+            if 'textSectionList' in entry:
+                textSectionList = entry['textSectionList']
+                for ts in textSectionList:
+                    if ts['textSection']['textSectionName'] == 'description':
+                        d = ts['textSection']['textSectionContent']
+                        #there are internal references to OMIM identifiers in the description, I am
+                        #formatting them in our style.
+                        d = re.sub('{(\\d+)}','OMIM:\\1',d)
+                        break
+        return d
+
+    def _get_omim_ids(self):
+        omimids = []
+
+        #an omim-specific thing here; from the omim.txt.gz file, get the omim numbers
+        #not unzipping the file
+        print("INFO: Obtaining OMIM record identifiers")
+        line_counter=0
+        omimfile=('/').join((self.rawdir,self.files['all']['file']))
+        with gzip.open(omimfile, 'rb') as f:
+            for line in f:
+                line=line.decode().strip()
+                if (line=="*FIELD* NO"):
+                   line_counter += 1
+                   #read the next line
+                   number=f.readline().decode().strip()
+                   omimids.append(number)
+
+        print("INFO: Done.  I found",omimids.__len__(),"omim ids")
+        return omimids
