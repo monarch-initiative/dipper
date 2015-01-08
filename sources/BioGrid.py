7c38d743c481d3dee5ae40323729ce79dddb42b7
Nicole Washington <nicole.l.washington@gmail.com>
2015-01-08 15:51:21 -0800

    can now processes BioGrid identifier mapping file.  addresses #21

--- sources/BioGrid.py
+++ sources/BioGrid.py
@@ -1,3 +1,6 @@
+from utils.CurieUtil import CurieUtil
+from utils.GraphUtils import GraphUtils
+
 __author__ = 'nicole'
 
 from rdflib import Namespace
@@ -31,12 +34,17 @@ class BioGrid(Source):
                            'url' : 'http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-ALL-LATEST.mitab.zip'
         },
         'identifiers' : {'file' : 'identifiers.tab.zip',
-                          'url' : 'http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-ALL-LATEST.tab.zip'
+                          'url' : 'http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-IDENTIFIERS-LATEST.tab.zip'
         }
     }
 
-    curie_map = {'NCBIGene' : 'http://www.ncbi.nlm.nih.gov/gene/',
-                 'BIOGRID' : 'http://thebiogrid.org/'}
+    curie_map = {
+        'NCBIGene' : 'http://www.ncbi.nlm.nih.gov/gene/',
+        'BIOGRID' : 'http://thebiogrid.org/',
+        'WormBase' : 'http://identifiers.org/WormBase:',
+        'MGI' : 'http://www.informatics.jax.org/accession/MGI:',
+        'ENSEMBL' : 'http://identifiers.org/ENSEMBL:'
+    }
 
 
     def __init__(self,args=[]):
@@ -90,7 +98,10 @@ class BioGrid(Source):
         :return: None
         '''
 
-        self._get_interactions(limit)
+        #TODO make each of these items an option... we may want to process them separately
+        #self._get_interactions(limit)
+
+        self._get_identifiers(limit)
 
         self.load_bindings()
 
@@ -156,12 +167,12 @@ class BioGrid(Source):
                 #TODO remove these filters, or parameterize them
                 #uncomment the following codeblock if you want to filter based on taxid
                 #taxids = [9606,10090,10116,7227,7955,6239,8355]  #our favorite animals
-                #taxids = [9606] #human
-                #if (not (taxids.__contains__(int(re.sub('taxid:','', taxid_a.rstrip()))) or
-                #    taxids.__contains__(int(re.sub('taxid:','', taxid_b.rstrip()))) )):
-                #    continue
-                #else:
-                #    matchcounter += 1
+                taxids = [9606] #human
+                if (not (taxids.__contains__(int(re.sub('taxid:','', taxid_a.rstrip()))) or
+                    taxids.__contains__(int(re.sub('taxid:','', taxid_b.rstrip()))) )):
+                    continue
+                else:
+                    matchcounter += 1
 
 
                 #TODO proper testing/catching of these search/match methods
@@ -198,6 +209,64 @@ class BioGrid(Source):
         #print("INFO: found",str(matchcounter),"matching rows")  #for testing
 
         return
+
+    def _get_identifiers(self,limit):
+        '''
+        This will process the id mapping file provided by Biogrid.
+        The file has a very large header, which we scan past, then pull the identifiers, and make
+        equivalence axioms
+        :param limit:
+        :return:
+        '''
+        line_counter = 0
+        f=('/').join((self.rawdir,self.files['identifiers']['file']))
+        myzip = ZipFile(f,'r')
+        #assume that the first entry is the item
+        fname=myzip.namelist()[0]
+        matchcounter=0
+        foundheader=False
+        with myzip.open(fname,'r') as csvfile:
+            for line in csvfile:
+                #skip header lines
+                if (not foundheader):
+                    if (re.match('BIOGRID_ID',line.decode())):
+                        foundheader=True
+                    continue
+
+                line=line.decode().strip()
+                #BIOGRID_ID	IDENTIFIER_VALUE	IDENTIFIER_TYPE	ORGANISM_OFFICIAL_NAME
+                #1	814566	ENTREZ_GENE	Arabidopsis thaliana
+                (biogrid_num,id_num,id_type,organism_label) = line.split('\t')
+
+                #for each one of these, create the node and add equivalent classes
+                cu = CurieUtil(self.curie_map)
+                gu = GraphUtils(self.curie_map)
+                g = self.graph
+                biogrid_id='BIOGRID:'+biogrid_num
+                prefix = self._map_idtype_to_prefix(id_type)
+
+
+                #geneidtypefilters='NCBIGene,OMIM,MGI,FlyBase,ZFIN,MGI,HGNC,WormBase,XenBase,ENSEMBL,miRBase'.split(',')
+                #TODO make these filters available as commandline options
+                geneidtypefilters='NCBIGene,MGI,ENSEMBL'.split(',')
+                #speciesfilters='Homo sapiens,Mus musculus,Drosophila melanogaster,Danio rerio,Caenorhabditis elegans,Xenopus laevis'.split(',')
+                speciesfilters='Homo sapiens,Mus musculus'.split(',')
+                #proteinidtypefilters='HPRD,Swiss-Prot,NCBIProtein'
+                if ((speciesfilters is not None) and (organism_label.strip() in speciesfilters)):
+                    line_counter += 1
+                    if ((geneidtypefilters is not None) and (prefix in geneidtypefilters)):
+                        mapped_id=(':').join((prefix,id_num))
+                        gu.addEquivalentClass(g,biogrid_id,mapped_id)
+                    elif (id_type == 'OFFICIAL_SYMBOL'):  #this symbol will only get attached to the biogrid class
+                        gu.addClassToGraph(g,biogrid_id,id_num)
+                    #elif (id_type == 'SYNONYM'):
+                    #    gu.addSynonym(g,biogrid_id,id_num)  #FIXME - i am not sure these are synonyms, altids?
+
+                if (limit is not None and line_counter > limit):
+                    break
+
+        return
+
 
     def _map_MI_to_RO(self,mi_id):
         mi_ro_map = {
@@ -239,3 +308,56 @@ class BioGrid(Source):
             print("WARN: unmapped code",mi_id, ". Defaulting to experimental_evidence")
 
         return eco_id
+
+    def _map_idtype_to_prefix(self,idtype):
+        prefix = idtype
+        idtype_to_prefix_map = {
+            'XENBASE' : 'XenBase',
+            'TREMBL' : 'TrEMBL',
+            'MGI' : 'MGI',
+            'REFSEQ_DNA_ACCESSION': 'RefSeqNA',
+            'MAIZEGDB' : 'MaizeGDB',
+            'BEEBASE' : 'BeeBase',
+            'ENSEMBL' : 'ENSEMBL',
+            'TAIR' : 'TAIR',
+            'GENBANK_DNA_GI' : 'NCBIgi',
+            'CGNC' : 'CGNC',
+            'RGD' : 'RGD',
+            'GENBANK_GENOMIC_DNA_GI' : 'NCBIgi',
+            'SWISSPROT' : 'Swiss-Prot',
+            'MIM' : 'OMIM',
+            'FLYBASE' : 'FlyBase',
+            'VEGA' : 'VEGA',
+            'ANIMALQTLDB' : 'AQTLDB',
+            'ENTREZ_GENE_ETG' : 'ETG',
+            'HPRD' : 'HPRD',
+            'APHIDBASE' : 'APHIDBASE',
+            'GENBANK_PROTEIN_ACCESSION' : 'NCBIProtein',
+            'ENTREZ_GENE' : 'NCBIGene',
+            'SGD' : 'SGD',
+            'GENBANK_GENOMIC_DNA_ACCESSION' : 'NCBIGenome',
+            'BGD' : 'BGD',
+            'WORMBASE' : 'WormBase',
+            'ZFIN' : 'ZFIN',
+            'DICTYBASE' : 'dictyBase',
+            'ECOGENE' : 'ECOGENE',
+            'BIOGRID' : 'BIOGRID',
+            'GENBANK_DNA_ACCESSION' : 'NCBILocus',
+            'VECTORBASE' : 'VectorBase',
+            'MIRBASE' : 'miRBase',
+            'IMGT/GENE-DB' : 'IGMT',
+            'HGNC' : 'HGNC',
+            'SYSTEMATIC_NAME' : None,
+            'OFFICIAL_SYMBOL' : None,
+            'REFSEQ_GENOMIC_DNA_ACCESSION' : 'NCBILocus',
+            'GENBANK_PROTEIN_GI' : 'NCBIgi',
+            'REFSEQ_PROTEIN_ACCESSION' : 'RefSeqProt',
+            'SYNONYM' : None,
+            'GRID_LEGACY' : None
+        }
+        if (idtype in idtype_to_prefix_map):
+            prefix = idtype_to_prefix_map.get(idtype)
+        else:
+            print("WARN: unmapped prefix",prefix)
+
+        return prefix
