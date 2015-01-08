47f4a7dc7a45fb365de979d4baa1e709bf1c1968
Nicole Washington <nicole.l.washington@gmail.com>
2015-01-07 17:31:00 -0800

    documentation for Panther;  change species filter to be AND instead of OR; update mgi/dicty curie maps

--- sources/Panther.py
+++ sources/Panther.py
@@ -14,8 +14,18 @@ from models.Dataset import Dataset
 
 class Panther(Source):
     '''
-    The pairwise orthology calls from Panther DB: http://pantherdb.org/
-    The orthology classes are mapped to RO relationships
+    The pairwise orthology calls from Panther DB: http://pantherdb.org/ encompass 22 species,
+    from the RefGenome and HCOP projects.
+    Here, we map the orthology classes to RO homology relationships
+    This resource may be extended in the future with additional species.
+
+    This currently makes a graph of orthologous relationships between genes, with the assumption that
+    gene metadata (labels, equivalent ids) are provided from other sources.
+
+    Gene families are nominally created from the orthology files, though these are incomplete with
+    no hierarchical (subfamily) information.  This will get updated from the HMM files in the future.
+
+    Note that there is a fair amount of identifier cleanup performed, to align with standard CURIE prefixes.
     '''
 
     files = {
@@ -31,18 +41,18 @@ class Panther(Source):
         'NCBIGene' : 'http://www.ncbi.nlm.nih.gov/gene/',
         'BIOGRID' : 'http://thebiogrid.org/',
         'ENSEMBL' : 'http://identifiers.org/ENSEMBL:',
-        'MGI': 'http://www.informatics.jax.org/',  #FIXME
+        'MGI': 'http://www.informatics.jax.org/accession/MGI:',  #All MGI ids are genes in this case
         'UniProtKB' : 'http://identifiers.org/UniProt:',
         'RGD' : 'http://rgd.mcw.edu/rgdweb/report/gene/main.html?id=',
         'ZFIN' : 'http://zfin.org/',
-        'dictyBase' : 'http://identifiers.org/dictyBase:',
+        'dictyBase' : 'http://dictybase.org/gene/',
         'TAIR' : 'http://identifiers.org/TAIR:',
         'FlyBase' : 'http://identifiers.org/FB:',
         'PomBase' : 'http://identifiers.org/PomBase:',
         'WormBase' : 'http://identifiers.org/WormBase:',
         'SGD' : 'http://identifiers.org/SGD:',
         'PANTHER' : 'http://www.pantherdb.org/panther/family.do?clsAccession=',
-        'DATA' : 'http://purl.obolibrary.org/DATA_'
+        'DATA' : 'http://purl.obolibrary.org/DATA_',
     }
 
 
@@ -66,8 +76,8 @@ class Panther(Source):
         '''
 
         self.get_files()
-
         #TODO the version number is tricky to get...we can't get it from redirects of the url
+        #TODO use the remote timestamp of the file?
 
         return
 
@@ -115,16 +125,27 @@ class Panther(Source):
         return status
 
     def _get_orthologs(self,limit):
+        '''
+        will process each of the specified pairwise orthology files, creating orthology associations
+        based on the specified orthology code.
+        this currently assumes that each of the orthology files is identically formatted.
+
+        there is also a nominal amount of identifier re-formatting:
+        MGI:MGI --> MGI
+        Ensembl --> ENSEMBL
+        :param limit:
+        :return:
+        '''
         line_counter = 0
         for k in self.files.keys():
             f=('/').join((self.rawdir,self.files[k]['file']))
             matchcounter=0
             mytar = tarfile.open(f,'r:gz')
+
             #assume that the first entry is the item
             fname=mytar.getmembers()[0]
-
+            print("INFO: Parsing",fname)
             with mytar.extractfile(fname) as csvfile:
-
                 for line in csvfile:
                     #skip comment lines
                     if (re.match('^#',line.decode())):
@@ -143,26 +164,26 @@ class Panther(Source):
                     gene_a = re.sub('=',':',gene_a)
                     gene_b = re.sub('=',':',gene_b)
 
-                    #special case for MGI
-                    gene_a = re.sub('MGI:MGI:','MGI:',gene_a)
-                    gene_b = re.sub('MGI:MGI:','MGI:',gene_b)
-
-                    #rewrite Ensembl --> ENSEMBL
-                    gene_a = re.sub('Ensembl','ENSEMBL',gene_a)
-                    gene_b = re.sub('Ensembl','ENSEMBL',gene_b)
+                    gene_a = self._clean_up_gene_id(gene_a,species_a)
+                    gene_b = self._clean_up_gene_id(gene_b,species_b)
 
                     taxon_a = self._map_taxon_abbr_to_id(species_a)
                     taxon_b = self._map_taxon_abbr_to_id(species_b)
 
                     #TODO remove these filters, or parameterize them
-                    #uncomment the following codeblock if you want to filter based on taxid
-                    #taxids = [9606,10090,10116,7227,7955,6239,8355]  #our favorite animals
-                    #taxids = [9606] #human
-                    #if (not (taxids.__contains__(int(re.sub('NCBITaxon:','', taxon_a.rstrip()))) or
-                    #    taxids.__contains__(int(re.sub('NCBITaxon:','', taxon_b.rstrip()))) )):
-                    #    continue
-                    #else:
-                    #    matchcounter += 1
+                    ###uncomment the following code block if you want to filter based on taxid
+                    taxids = [9606,10090,10116,7227,7955,6239,8355]  #our favorite animals
+                    #taxids = [9606,10090] #human/mouse only
+                    #taxids = [9606] #human only
+                    #retain only those orthologous relationships to genes in the specified taxids
+                    #using AND will get you only those associations where gene1 AND gene2 are in the taxid list (most-filter)
+                    #using OR will get you any associations where gene1 OR gene2 are in the taxid list (some-filter)
+                    if (not (taxids.__contains__(int(re.sub('NCBITaxon:','', taxon_a.rstrip()))) and
+                        taxids.__contains__(int(re.sub('NCBITaxon:','', taxon_b.rstrip()))) )):
+                        continue
+                    else:
+                        matchcounter += 1
+                    ###end code block for filtering on taxon
 
                     rel=self._map_orthology_code_to_RO(orthology_class)
 
@@ -191,7 +212,7 @@ class Panther(Source):
         '''
         Will map the panther-specific taxon abbreviations to NCBI taxon numbers
         :param ptax:
-        :return:
+        :return: NCBITaxon id
         '''
         taxid = None
         ptax_to_taxid_map = {
@@ -226,13 +247,19 @@ class Panther(Source):
         return taxid
 
     def _map_orthology_code_to_RO(self,ortho):
-        ro_id = 'RO:HOM0000017' #in orthology relationship with
+        '''
+        Map the panther-specific orthology code (P,O,LDO,X,LDX) to relationship-ontology
+        identifiers.
+        :param ortho: orthology code
+        :return: RO identifier
+        '''
+        ro_id = OrthologyAssoc.relationships['orthologous'] #in orthology relationship with
         ortho_to_ro_map = {
-            'P': 'RO:HOM0000011', ## in paralogy relationship with
-            'O': 'RO:HOM0000037', ## in 1 to many homology relationship with
-            'LDO': 'RO:HOM0000019', ## in 1 to 1 homology relationship with
-            'X': 'RO:HOM0000018', ## in xenology relationship with
-            'LDX': 'RO:HOM0000018' ## in xenology relationship with
+            'P' : OrthologyAssoc.relationships['paralogous'],
+            'O' : OrthologyAssoc.relationships['orthologous'],
+            'LDO': OrthologyAssoc.relationships['least_diverged_orthologous'],
+            'X': OrthologyAssoc.relationships['xenologous'],
+            'LDX': OrthologyAssoc.relationships['xenologous']
         }
 
         if (ortho in ortho_to_ro_map):
@@ -241,3 +268,41 @@ class Panther(Source):
             print("WARN: unmapped orthology code",ortho,". Defaulting to 'orthology'.")
 
         return ro_id
+
+    def _clean_up_gene_id(self,id,sp):
+        '''
+        A series of identifier rewriting to conform with standard gene identifiers.
+        :param id:
+        :param sp:
+        :return:
+        '''
+        #special case for MGI
+        id = re.sub('MGI:MGI:','MGI:',id)
+
+        #rewrite Ensembl --> ENSEMBL
+        id = re.sub('Ensembl','ENSEMBL',id)
+
+        #rewrite Gene:CELE --> WormBase  these are old-school cosmid identifier
+        id = re.sub('Gene:CELE','WormBase:',id)
+        if (sp == 'CAEEL'):
+            if (re.match('(Gene|ENSEMBLGenome):\w+\\.\d+',id)):
+                id = re.sub('(?:Gene|ENSEMBLGenome):(\w+\\.\d+)','WormBase:\\1',id)
+
+        #rewrite GeneID --> NCBIGene
+        id = re.sub('GeneID','NCBIGene',id)
+
+        #rewrite Gene:Dmel --> FlyBase
+        id = re.sub('Gene:Dmel_','FlyBase:',id)
+        #rewrite Gene:CG --> FlyBase:CG
+        id = re.sub('Gene:CG','FlyBase:CG',id)
+
+        #rewrite ENSEMBLGenome:FBgn --> FlyBase:FBgn
+        id = re.sub('ENSEMBLGenome:FBgn','FlyBase:FBgn',id)
+
+        #rewrite Gene:<ensembl ids> --> ENSEMBL:<id>
+        id = re.sub('Gene:ENS','ENSEMBL:ENS',id)
+
+        if (re.match('Gene:',id)):
+            print("WARN: Found something I don't know how to fix ( species",sp,"):",id)
+
+        return id
