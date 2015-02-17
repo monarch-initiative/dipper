96a22a7fc4e5b3809d5ecce433193d0e9a85525e
kshefchek <kshefchek@gmail.com>
2015-02-17 10:39:16 -0800

    Break out dipper tests main function into one file

--- tests/IMPCTests.py
+++ tests/IMPCTests.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
-
 from sources.IMPC import IMPC
 from utils.TestUtils import TestUtils
 import logging
@@ -12,17 +5,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def main():
-    compare_checksums()
-
 def compare_checksums():
     """
     test to see if fetched file matches checksum from ebi
     :return: True or False
     """
-    logger.info('Comparing IMPC files to EBI Checksums')
-    is_foo = True
+    is_match = True
     impc = IMPC()
+    impc.rawdir = "../raw/impc"
     impc.fetch(False)
     reference_checksums = impc.parse_checksum_file(
         impc.files['checksum']['file'])
@@ -34,9 +24,4 @@ def compare_checksums():
                         ' was not downloaded completely')
             return is_match
 
-    logger.info('PASSED: Files have same checksum as reference')
-
-if __name__ == "__main__":
-    main()
-
-
+    return is_match
