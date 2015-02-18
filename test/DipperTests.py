a5d7ebaac912882a34ca31a73322e1fab39f5cad
kshefchek <kshefchek@gmail.com>
2015-02-18 12:19:38 -0800

    add example of unittest for genotype class

--- test/DipperTests.py
+++ test/DipperTests.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-
+"""
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
+"""
 
 from models.Genotype import Genotype
 import unittest
@@ -16,13 +17,19 @@ logger = logging.getLogger(__name__)
 class GenotypeTestCase(unittest.TestCase):
 
     def setUp(self):
-        genotype_id = 'MGI:5515892'
-        genotype_label = 'Pmp22<Tr-2J>/Pmp22<+> [C57BL/6J-Pmp22<Tr-2J>/GrsrJ]'
-        self.genotype = Genotype('The widget')
+        self.genotype_id = 'MGI:5515892'
+        self.genotype_label = \
+            'Pmp22<Tr-2J>/Pmp22<+> [C57BL/6J-Pmp22<Tr-2J>/GrsrJ]'
+        self.genotype = Genotype(self.genotype_id, self.genotype_label)
 
     def tearDown(self):
-        self.genotype.dispose()
         self.genotype = None
+
+    def test_genotype_constructor(self):
+        from rdflib.namespace import RDFS
+        from rdflib import Literal
+        self.assertTrue((self.genotype.geno, RDFS['label'],
+                         Literal(self.genotype_label)) in self.genotype.g)
 
 
 if __name__ == '__main__':
