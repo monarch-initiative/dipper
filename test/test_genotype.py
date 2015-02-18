df8c4913b2743fee13632c1fa5b34f88e6d39365
kshefchek <kshefchek@gmail.com>
2015-02-18 12:29:23 -0800

    rename unittest for command line (so python3 -m unittest with defaults works)

--- test/test_genotype.py
+++ test/test_genotype.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""
+
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-"""
+
 
 from models.Genotype import Genotype
 import unittest
