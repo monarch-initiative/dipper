5324c712df6bc652627d1da0cdbb199a2b95a3c1
kshefchek <kshefchek@gmail.com>
2015-02-19 12:32:49 -0800

    playing around with setup for importing

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+setup(name='DIPper',
+      version='1.0',
+      description='Data Ingest Pipeline',
+      packages=['distutils', 'distutils.command,psycopg2, rdflib, isodate, roman, python-docx, pyyaml'],
+)
