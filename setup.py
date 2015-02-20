001d993e9b59faa6d828508b9783b70b66c2eacb
kshefchek <kshefchek@gmail.com>
2015-02-19 16:37:55 -0800

    update setup for mckb

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ from distutils.core import setup
 setup(name='DIPper',
       version='1.0',
       description='Data Ingest Pipeline',
-      packages=['dipper','dipper.sources','dipper.models','dipper.utils'],
+      packages=['dipper', 'dipper.sources', 'dipper.models', 'dipper.utils'],
+      package_dir={'dipper':'/', 'dipper.sources': '/',
+                   'dipper.models':'/', 'dipper.utils':'/'
+                   },
+      install_requires=['psycopg2', 'rdflib', 'isodate', 'roman', 'python-docx', 'pyyaml']
       )
