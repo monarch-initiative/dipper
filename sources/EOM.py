87845753070c3922be3c0f75e8c2d8cde7a798cb
Nicole Washington <nicole.l.washington@gmail.com>
2015-02-22 12:42:23 -0800

    refactored EOM
    *removed rdflib calls.  closes #62
    *added documentation and cleaned up code.

--- sources/EOM.py
+++ sources/EOM.py
@@ -19,20 +19,27 @@ from utils.GraphUtils import GraphUtils
 logger = logging.getLogger(__name__)
 
 class EOM(Source):
-    '''
+    """
+    Elements of Morphology is a great resource from NHGRI that has definitions of morphological abnormalities,
+    together with image depictions.  We pull those relationships, as well as our local mapping of equivalences
+    between EOM and HP terminologies.
+
+    The website is crawled monthly by NIF's DISCO crawler system, which we utilize here.
     Be sure to have pg user/password connection details in your conf.json file, like:
       dbauth : {
         'disco' : {'user' : '<username>', 'password' : '<password>'}
       }
-    '''
+
+    Hand-curated data for the HP to EOM mapping is currently stored offline, and will need to be placed into
+    the raw/ directory here.  TODO: move this into a public repo.
+
+    """
+
+    #we are using the dv view here; i wonder if we should be using the prod view, or their services instead.?
     tables = [
         'dv.nlx_157874_1'
     ]
 
-    terms = {
-        'phenotype': 'MONARCH:phenotype'  # Is this correct? What about GENO:0000348 - phenotype? MONARCH:phenotype
-
-    }
 
     relationship = {
         'hasRelatedSynonym': 'OIO:hasRelatedSynonym',
@@ -79,21 +86,7 @@ class EOM(Source):
 
         return
 
-    def scrub(self):
-        '''
-        Perform various data-scrubbing on the raw data files prior to parsing.
-        For this resource, this currently includes:
 
-        :return: None
-        '''
-        # scrub file of the oddities where there are "\" instead of empty strings
-        #pysed.replace("\r", '', ('/').join((self.rawdir,'dv.nlx_157874_1')))
-
-        return
-
-    # here we're reading and building a full named graph of this resource, then dumping it all at the end
-    # we can investigate doing this line-by-line later
-    # supply a limit if you want to test out parsing the head X lines of the file
     def parse(self, limit=None):
         if (limit is not None):
             logger.info("Only parsing first %s rows of each file", limit)
@@ -115,15 +108,26 @@ class EOM(Source):
 
 
     def _process_nlx_157874_1_view(self, raw, limit=None):
-        '''
+        """
         This table contains the Elements of Morphology data that has been screen-scraped into DISCO.
+        Note that foaf:depiction is inverse of foaf:depicts relationship.
+
+        Triples:
+            <eom id> a owl:Class
+                rdf:label Literal(eom label)
+                OIO:hasRelatedSynonym Literal(synonym list)
+                IAO:definition Literal(subjective def), Literal(objective_def)
+                foaf:depiction Literal(small_image_url),Literal(large_image_url)
+                foaf:page Literal(page_url)
+                dc:comment Literal(long commented text)
+
+
         :param raw:
         :param limit:
         :return:
-        '''
+        """
 
         gu = GraphUtils(curie_map.get())
-        cu = CurieUtil(curie_map.get())
         line_counter = 0
         with open(raw, 'r') as f1:
             f1.readline()  # read the header row; skip
@@ -135,63 +139,64 @@ class EOM(Source):
                  subjective_definition, comments, synonyms, replaces, small_figure_url, large_figure_url,
                  e_uid, v_uid, v_uuid, v_last_modified) = line.split('\t')
 
-                #Assemble the description text
-                if subjective_definition != '' and objective_definition == '':
-                    description = 'Subjective Description: '+subjective_definition
-                elif objective_definition != '' and subjective_definition == '':
-                    description = 'Objective Description: '+objective_definition
-                elif subjective_definition != '' and objective_definition != '':
-                    description = 'Objective Description: '+objective_definition+'; Subjective Description: '+subjective_definition
-                else:
-                    description = None
-
                 #Add morphology term to graph as a class with label, type, and description.
-                gu.addClassToGraph(self.graph,morphology_term_id,morphology_term_label,self.terms['phenotype'],description)
+                gu.addClassToGraph(self.graph,morphology_term_id,morphology_term_label)
 
+                #Assemble the description text
+                description = None
+                if subjective_definition.strip() != '':
+                    subjective_definition = 'Subjective Description: '+subjective_definition.strip()
+                    gu.addDefinition(self.graph,morphology_term_id,subjective_definition)
+                if objective_definition.strip() != '':
+                    objective_definition = 'Objective Description: '+objective_definition.strip()
+                    gu.addDefinition(self.graph,morphology_term_id,objective_definition)
+
+
+                #<term id> FOAF:depicted_by literal url
+                #<url> type foaf:depiction
+
+                #do we want both images?
                 #morphology_term_id has depiction small_figure_url
                 if small_figure_url != '':
-                    self.graph.add((URIRef(cu.get_uri(morphology_term_id)),FOAF['depiction'],Literal(small_figure_url)))
+                    gu.addDepiction(self.graph,morphology_term_id,small_figure_url)
 
                 #morphology_term_id has depiction large_figure_url
                 if large_figure_url != '':
-                    self.graph.add((URIRef(cu.get_uri(morphology_term_id)),FOAF['depiction'],Literal(large_figure_url)))
+                    gu.addDepiction(self.graph,morphology_term_id,large_figure_url)
 
                 #morphology_term_id has comment comments
                 if comments != '':
-                    self.graph.add((URIRef(cu.get_uri(morphology_term_id)),DC['comment'],Literal(comments)))
+                    gu.addComment(self.graph,morphology_term_id,comments.strip())
 
-                #morphology_term_id hasRelatedSynonym synonyms (; delimited)
+
                 if synonyms != '':
-                    items = synonyms.split(';')
-                    for i in items:
-                        self.graph.add((URIRef(cu.get_uri(morphology_term_id)),URIRef(cu.get_uri(self.relationship['hasRelatedSynonym'])),Literal(i)))
+                    for s in synonyms.split(';'):
+                        gu.addSynonym(self.graph,morphology_term_id,s.strip())
 
                 #morphology_term_id hasRelatedSynonym replaces (; delimited)
                 if replaces != '' and replaces != synonyms:
-                    items = replaces.split(';')
-                    for i in items:
-                        self.graph.add((URIRef(cu.get_uri(morphology_term_id)),URIRef(cu.get_uri(self.relationship['hasRelatedSynonym'])),Literal(i)))
+                    for s in replaces.split(';'):
+                        gu.addSynonym(self.graph,morphology_term_id,s.strip())
 
-                #FIXME: What is the proper predicate for a web page link? morphology_term_id hasURL morphology_term_url?
                 #morphology_term_id has page morphology_term_url
-                self.graph.add((URIRef(cu.get_uri(morphology_term_id)),FOAF['page'],Literal(morphology_term_url)))
-
+                gu.addPage(self.graph,morphology_term_id,morphology_term_url)
 
                 if (limit is not None and line_counter > limit):
                     break
-                #self.graph = eom.getGraph().__iadd__(self.graph)
         return
 
     def _map_eom_terms(self, raw, limit=None):
-        '''
+        """
         This table contains the HP ID mappings from the local tsv file.
+        Triples:
+            <eom id> owl:equivalentClass <hp id>
         :param raw:
         :param limit:
         :return:
-        '''
+        """
 
         gu = GraphUtils(curie_map.get())
-        cu = CurieUtil(curie_map.get())
+
         line_counter = 0
         with open(raw, 'r') as f1:
             f1.readline()  # read the header row; skip
@@ -203,6 +208,8 @@ class EOM(Source):
                 #Sub out the underscores for colons.
                 hp_id = re.sub('_', ':', hp_id)
                 if re.match(".*HP:.*", hp_id):
+                    #add the HP term as a class
+                    gu.addClassToGraph(self.graph,hp_id,None)
                     #Add the HP ID as an equivalent class
                     gu.addEquivalentClass(self.graph,morphology_term_id,hp_id)
                 else:
@@ -212,35 +219,5 @@ class EOM(Source):
                     break
 
         return
-
-
-
-
-
-    #TODO generalize this to a set of utils
-    def _getcols(self,cur,table):
-        query=(' ').join(("SELECT * FROM",table,"LIMIT 0"))  #for testing
-        #print("COMMAND:",query)
-        cur.execute(query)
-        colnames = [desc[0] for desc in cur.description]
-        logger.info("COLS (%s): %s", table, colnames)
-
-        return
-
-
-    def file_len(self,fname):
-        with open(fname) as f:
-            return sum(1 for line in f)
-
-
-
-
-    def verify(self):
-        status = False
-        self._verify(self.outfile)
-        status = self._verifyowl(self.outfile)
-
-        # verify some kind of relationship that should be in the file
-        return status
 
 
