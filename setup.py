4d51086e70a45f7ebc53ed562e15219e5e8e36d0
kshefchek <kshefchek@gmail.com>
2015-03-25 16:01:50 -0700

    updating setup py for manifest file

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='Dipper',
       version='0.0.1',
       description='Data Ingest Pipeline',
       packages=find_packages(),
-      install_requires=['psycopg2', 'rdflib', 'isodate', 'roman', 'python-docx', 'pyyaml','pysftp']
+      install_requires=['psycopg2', 'rdflib', 'isodate', 'roman', 'python-docx', 'pyyaml','pysftp'],
+      include_package_data=True
       )
