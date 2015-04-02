4899acc30a6a123e001a648a28f8389b5dc249ac
Nicole Washington <nicole.l.washington@gmail.com>
2015-04-02 15:02:24 -0700

    refactor with test set #91

--- dipper/sources/HPOAnnotations.py
+++ dipper/sources/HPOAnnotations.py
@@ -3,13 +3,16 @@ import os
 from datetime import datetime
 from stat import *
 import urllib
+import logging
 
 from dipper.utils import pysed
+from dipper.utils.GraphUtils import GraphUtils
 from dipper.sources.Source import Source
 from dipper.models.D2PAssoc import D2PAssoc
 from dipper.models.DispositionAssoc import DispositionAssoc
 from dipper.models.Dataset import Dataset
 from dipper.models.Assoc import Assoc
+from dipper import curie_map
 
 
 '''
@@ -31,6 +34,7 @@ from dipper.models.Assoc import Assoc
 #14 	Assigned by 	required 	HPO
 '''
 
+logger = logging.getLogger(__name__)
 
 class HPOAnnotations(Source):
 
@@ -52,6 +56,14 @@ class HPOAnnotations(Source):
         "TAS": "ECO:0000304"   #Traceable Author Statement
     }
 
+    test_ids = ['OMIM:119600','OMIM:120160','OMIM:157140','OMIM:158900',
+                'OMIM:166220','OMIM:168600','OMIM:219700','OMIM:253250',
+                'OMIM:305900','OMIM:600669','OMIM:601278','OMIM:602421',
+                'OMIM:605073','OMIM:607822',  #from coriell
+                'Orphanet:99889','Orphanet:99','DECIPHER:14','DECIPHER:1',
+                'OMIM:194072','OMIM:100100','Orphanet:99798']  #these ones xref the other disease DBs
+
+
     def __init__(self):
         Source.__init__(self, 'hpoa')
 
@@ -60,7 +72,7 @@ class HPOAnnotations(Source):
         self.dataset = Dataset('hpoa', 'Human Phenotype Ontology', 'http://www.human-phenotype-ontology.org')
 
         #data-source specific warnings (will be removed when issues are cleared)
-        print("WARN: note that some ECO classes are missing for ICE and PCS; using temporary mappings.")
+        logger.warn("note that some ECO classes are missing for ICE and PCS; using temporary mappings.")
 
         return
 
@@ -95,22 +107,22 @@ class HPOAnnotations(Source):
         '''
         # scrub file of the oddities...lots of publication rewriting
         f = ('/').join((self.rawdir,self.files['annot']['file']))
-        print('INFO: scrubbing PubMed:12345 --> PMID:12345')
+        logger.info('scrubbing PubMed:12345 --> PMID:12345')
         pysed.replace("PubMed", 'PMID', f)
 
-        print('INFO: scrubbing pmid:12345 --> PMID:12345')
+        logger.info('scrubbing pmid:12345 --> PMID:12345')
         pysed.replace("pmid", 'PMID', f)
 
-        print('INFO: scrubbing PMID12345 --> PMID:12345')
+        logger.info('scrubbing PMID12345 --> PMID:12345')
         pysed.replace("PMID([0-9][0-9]*)", 'PMID:\\1', f)
 
-        print('INFO: scrubbing MIM12345 --> OMIM:12345')
+        logger.info('scrubbing MIM12345 --> OMIM:12345')
         pysed.replace('MIM([0-9][0-9]*)', 'OMIM:\\1', f)
 
-        print('INFO: scrubbing MIM:12345 --> OMIM:12345')
+        logger.info('scrubbing MIM:12345 --> OMIM:12345')
         pysed.replace(";MIM",";OMIM", f)
 
-        print ('INFO: scrubbing ORPHANET --> Orphanet')
+        logger.info('scrubbing ORPHANET --> Orphanet')
         pysed.replace("ORPHANET","Orphanet", f)
         return
 
@@ -120,18 +132,21 @@ class HPOAnnotations(Source):
     #supply a limit if you want to test out parsing the head X lines of the file
     def parse(self, limit=None):
         if (limit is not None):
-            print("Only parsing first", limit, "rows")
+            logger.info("Only parsing first %s rows", limit)
 
-        print("Parsing files...")
-        self._process_phenotype_tab(('/').join((self.rawdir,self.files['annot']['file'])),self.outfile,self.graph,limit)
-        Assoc().loadAllProperties(self.graph)
+        logger.info("Parsing files...")
+        for testMode in [True,False]:
+            self._process_phenotype_tab(('/').join((self.rawdir,self.files['annot']['file'])),limit,testMode)
+
+        for g in [self.graph,self.testgraph]:
+            Assoc().loadAllProperties(g)
 
         #TODO add negative phenotype statements
         #self._process_negative_phenotype_tab(self.rawfile,self.outfile,limit)
 
-        print("Finished parsing.")
+        logger.info("Finished parsing.")
 
-        print("Loaded", len(self.graph), "nodes")
+        logger.info("Loaded %d nodes", len(self.graph))
         return
 
 
@@ -144,8 +159,14 @@ class HPOAnnotations(Source):
         '''
         return self.eco_dict.get(code_string)
 
-    def _process_phenotype_tab(self, raw, out, g, limit=None):
+    def _process_phenotype_tab(self, raw, limit, testMode):
+        if (testMode):
+            g = self.testgraph
+            logging.info("Processing testset.")
+        else:
+            g = self.graph
         line_counter = 0
+        gu = GraphUtils(curie_map.get())
         with open(raw, 'r', encoding="utf8") as csvfile:
             filereader = csv.reader(csvfile, delimiter='\t', quotechar='\"')
             for row in filereader:
@@ -153,32 +174,39 @@ class HPOAnnotations(Source):
                 (db, num, name, qual, pheno_id, publist, eco, onset, freq, w, asp, syn, date, curator) = row
                 disease_id = db + ":" + num
 
+                if testMode and (disease_id not in self.test_ids):
+                    continue
+
                 #blow these apart if there is a list of pubs
                 publist=publist.split(';')
                 for pub in publist:
                     pub = pub.strip()
                     assoc_id = self.make_id(db + num + qual + pheno_id + pub + eco + onset + freq + date + curator)
                     assoc = None
+                    eco_id = self._map_evidence_to_codes(eco)
+                    #make sure to add the disease, phenotype, eco, as classes.
+                    #pub as individual is taken care of in the association function
+                    gu.addClassToGraph(g,disease_id,None)
+                    gu.addClassToGraph(g,pheno_id,None)
+                    gu.addClassToGraph(g,eco_id,None)
+
                     # we want to do things differently depending on the aspect of the annotation
                     if (asp == 'O' or asp == 'M'):  #organ abnormality or mortality
-                        assoc = D2PAssoc(assoc_id, disease_id, pheno_id, onset, freq, pub, self._map_evidence_to_codes(eco))
+                        assoc = D2PAssoc(assoc_id, disease_id, pheno_id, onset, freq, pub, eco_id)
                         g = assoc.addAssociationNodeToGraph(g)
                     elif (asp == 'I'):  #inheritance patterns for the whole disease
-                        assoc = DispositionAssoc(assoc_id, disease_id, pheno_id, pub, self._map_evidence_to_codes(eco))
+                        assoc = DispositionAssoc(assoc_id, disease_id, pheno_id, pub, eco_id)
                         g = assoc.addAssociationNodeToGraph(g)
                     elif (asp == 'C'):  #clinical course / onset
                         #FIXME is it correct for these to be dispositions?
-                        assoc = DispositionAssoc(assoc_id, disease_id, pheno_id, pub, self._map_evidence_to_codes(eco))
+                        assoc = DispositionAssoc(assoc_id, disease_id, pheno_id, pub, eco_id)
                         g = assoc.addAssociationNodeToGraph(g)
                     else:
                         #TODO throw an error?
-                        print("I don't know what this is:", asp)
+                        logger.error("I don't know what this aspect is:", asp)
 
-                    #if (assoc is not None):
-                    #   self.triple_count += assoc.printTypes(filewriter)
-                    #    self.triple_count += assoc.printAssociation(filewriter)
 
-                if (limit is not None and line_counter > limit):
+                if not testMode and (limit is not None and line_counter > limit):
                    break
 
         return
