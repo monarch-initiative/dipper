45e77e7b9e87a2c80e5d9dd3ff44173d969e8608
kshefchek <kshefchek@gmail.com>
2015-04-08 18:53:19 -0700

    add made up GENO terms as place holders

--- dipper/models/Genotype.py
+++ dipper/models/Genotype.py
@@ -45,12 +45,16 @@ class Genotype():
         'wildtype' : 'GENO:0000511',
         'reagent_targeted_gene': 'GENO:0000504',
         'missense_variant': 'SO:0001583',
-        'primary_transcript': 'SO:0000185',
-        'transcript_secondary_structure_variant': 'SO:0001596',
         'sequence_variant_causing_loss_of_function_of_polypeptide': 'SO:1000118',
         'sequence_variant_causing_gain_of_function_of_polypeptide': 'SO:1000125',
         'sequence_variant_causing_inactive_catalytic_site': 'SO:1000120',
-        'sequence_variant_affecting_polypeptide_function': 'SO:1000117'
+        'sequence_variant_affecting_polypeptide_function': 'SO:1000117',
+        'primary_priority': 'GENO:primary', # Made up term
+        'secondary_priority': 'GENO:secondary', #Made up term
+        'reference_nucleotide': 'GENO:reference_nucleotide', #Made up term
+        'reference_amino_acid': 'GENO:reference_amino_acid', #Made up term
+        'altered_nucleotide': 'GENO:altered_nucleotide', #Made up term
+        'results_in_amino_acid_change': 'GENO:results_in_amino_acid_change' #Made up term
     }
 
     object_properties = {
