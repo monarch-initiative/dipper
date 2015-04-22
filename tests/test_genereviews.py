a4d575a029df6c103e00adc180a2ffca13849d75
Nicole Washington <nicole.l.washington@gmail.com>
2015-04-22 13:38:33 -0700

    unit testing for GeneReviews
    * general code cleanup
    * unit tests written #91
    * print --> logging #17

---
+++ tests/test_genereviews.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from dipper.sources.GeneReviews import GeneReviews
+from tests.test_source import SourceTestCase
+
+import unittest
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+class GeneReviewsTestCase(SourceTestCase):
+
+    def setUp(self):
+        self.source = GeneReviews()
+        self.source.settestonly(True)
+        self._setDirToSource()
+        return
+
+    def tearDown(self):
+        self.source = None
+        return
+
+    # TODO add some specific tests to make sure we are hitting all parts of the code
+    #@unittest.skip('test not yet defined')
+    #def test_genereviewstest(self):
+    #    logger.info("A GeneReviews-specific test")
+    #
+    #    return
+
+
+if __name__ == '__main__':
+    unittest.main()
