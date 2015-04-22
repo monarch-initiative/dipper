2725baa64e6caa89717c2c1147f347183bb80841
Nicole Washington <nicole.l.washington@gmail.com>
2015-04-22 12:47:55 -0700

    unit testing for Panther
    * general code cleanup
    * unit tests written #91
    * print --> logging #17

--- dipper/sources/Panther.py
+++ dipper/sources/Panther.py
@@ -2,12 +2,15 @@ __author__ = 'nicole'
 
 import tarfile
 import re
+import logging
 
 from dipper.sources.Source import Source
 from dipper.models.OrthologyAssoc import OrthologyAssoc
 from dipper.models.Dataset import Dataset
 from dipper.utils.GraphUtils import GraphUtils
 from dipper import config, curie_map
+
+logger = logging.getLogger(__name__)
 
 
 class Panther(Source):
@@ -30,12 +33,10 @@ class Panther(Source):
     """
 
     files = {
-        'refgenome' : {'file': 'RefGenomeOrthologs.tar.gz',
-                           'url' : 'ftp://ftp.pantherdb.org/ortholog/current_release/RefGenomeOrthologs.tar.gz'
-        },
-        'hcop' : {'file' : 'orthologs_HCOP.tar.gz',
-                          'url' : 'ftp://ftp.pantherdb.org/ortholog/current_release/orthologs_HCOP.tar.gz'
-        }
+        'refgenome': {'file': 'RefGenomeOrthologs.tar.gz',
+                      'url': 'ftp://ftp.pantherdb.org/ortholog/current_release/RefGenomeOrthologs.tar.gz'},
+        'hcop': {'file': 'orthologs_HCOP.tar.gz',
+                 'url': 'ftp://ftp.pantherdb.org/ortholog/current_release/orthologs_HCOP.tar.gz'}
     }
 
     def __init__(self, tax_ids=None):
@@ -43,20 +44,19 @@ class Panther(Source):
         self.tax_ids = tax_ids
         self.load_bindings()
 
-        self.dataset = Dataset('panther', 'Protein ANalysis THrough Evolutionary Relationships', 'http://pantherdb.org/')
+        self.dataset = Dataset('panther', 'Protein ANalysis THrough Evolutionary Relationships',
+                               'http://pantherdb.org/')
 
         # Defaults
         if self.tax_ids is None:
             self.tax_ids = [9606, 10090]
 
-        if (not (('test_ids' in config.get_config()) and ('protein' in config.get_config()['test_ids']))):
-            print("WARN: not configured with gene test ids.")
+        if 'test_ids' not in config.get_config() and 'protein' not in config.get_config()['test_ids']:
+            logger.warn("not configured with gene test ids.")
         else:
             self.test_ids = config.get_config()['test_ids']['protein']
 
-
-
-        #data-source specific warnings (will be removed when issues are cleared)
+        # data-source specific warnings (will be removed when issues are cleared)
 
         return
 
@@ -66,34 +66,30 @@ class Panther(Source):
         """
 
         self.get_files(is_dl_forced)
-        #TODO the version number is tricky to get...we can't get it from redirects of the url
-        #TODO use the remote timestamp of the file?
+        # TODO the version number is tricky to get...we can't get it from redirects of the url
+        # TODO use the remote timestamp of the file?
 
         return
 
-    def parse(self,limit=None):
+    def parse(self, limit=None):
         """
         abstract method to parse all data from an external resource, that was fetched in
         fetch()
         :return: None
         """
 
-        loops = [True]
-        if not self.testOnly:
-            loops = [True,False]
+        if self.testOnly:
+            self.testMode = True
 
-        for testMode in loops:
-            self._get_orthologs(limit,testMode)
+        self._get_orthologs(limit)
 
         self.load_bindings()
 
-        print("INFO: Found", len(self.graph), "nodes")
-
+        logger.info("INFO: Found %d nodes", len(self.graph))
 
         return
 
-
-    def _get_orthologs(self,limit,testMode):
+    def _get_orthologs(self, limit):
         """
         This will process each of the specified pairwise orthology files, creating orthology associations
         based on the specified orthology code.
@@ -129,10 +125,10 @@ class Panther(Source):
         :param limit:
         :return:
         """
-        print("INFO: getting orthologs")
+        logger.info("getting orthologs")
         line_counter = 0
 
-        if testMode:
+        if self.testMode:
             g = self.testgraph
 
         else:
@@ -141,194 +137,201 @@ class Panther(Source):
         gu = GraphUtils(curie_map.get())
 
         for k in self.files.keys():
-            f=('/').join((self.rawdir,self.files[k]['file']))
-            matchcounter=0
-            mytar = tarfile.open(f,'r:gz')
+            f = '/'.join((self.rawdir, self.files[k]['file']))
+            matchcounter = 0
+            mytar = tarfile.open(f, 'r:gz')
 
-            #assume that the first entry is the item
-            fname=mytar.getmembers()[0]
-            print("INFO: Parsing",fname.name)
+            # assume that the first entry is the item
+            fname = mytar.getmembers()[0]
+            logger.info("Parsing %s", fname.name)
             with mytar.extractfile(fname) as csvfile:
                 for line in csvfile:
-                    #skip comment lines
-                    if (re.match('^#',line.decode())):
-                        print("INFO: Skipping header line")
+                    # skip comment lines
+                    if re.match('^#', line.decode()):
+                        logger.info("Skipping header line")
                         continue
                     line_counter += 1
-                    line=line.decode().strip()
-                    #print(line)
+                    line = line.decode().strip()
 
-                    #parse each row
+                    # parse each row
                     # HUMAN|Ensembl=ENSG00000184730|UniProtKB=Q0VD83	MOUSE|MGI=MGI=2176230|UniProtKB=Q8VBT6	LDO	Euarchontoglires	PTHR15964
-                    (a, b, orthology_class, ancestor_taxon,panther_id) = line.split('\t')
-                    (species_a,gene_a,protein_a) = a.split('|')
-                    (species_b,gene_b,protein_b) = b.split('|')
+                    (a, b, orthology_class, ancestor_taxon, panther_id) = line.split('\t')
+                    (species_a, gene_a, protein_a) = a.split('|')
+                    (species_b, gene_b, protein_b) = b.split('|')
 
-                    #skip the entries that don't have homolog relationships with the test ids
-                    if testMode and not (re.sub('UniProtKB=','',protein_a) in self.test_ids or
-                        re.sub('UniProtKB=','',protein_b) in self.test_ids):
+                    # skip the entries that don't have homolog relationships with the test ids
+                    if self.testMode and not (re.sub('UniProtKB=', '', protein_a) in self.test_ids or
+                                                 re.sub('UniProtKB=', '', protein_b) in self.test_ids):
                         continue
 
-
-                    #map the taxon abbreviations to ncbi taxon ids
+                    # map the taxon abbreviations to ncbi taxon ids
                     taxon_a = self._map_taxon_abbr_to_id(species_a)
                     taxon_b = self._map_taxon_abbr_to_id(species_b)
 
-                    #TODO remove these filters, or parameterize them
-                    ###uncomment the following code block if you want to filter based on taxid
-                    #taxids = [9606,10090,10116,7227,7955,6239,8355]  #our favorite animals
-                    #taxids = [9606] #human only
-                    #retain only those orthologous relationships to genes in the specified taxids
-                    #using AND will get you only those associations where gene1 AND gene2 are in the taxid list (most-filter)
-                    #using OR will get you any associations where gene1 OR gene2 are in the taxid list (some-filter)
-                    #print("INFO: restricting taxa to ids:",taxids)
-                    if ((int(re.sub('NCBITaxon:','', taxon_a.rstrip())) not in self.tax_ids ) and
-                        (int(re.sub('NCBITaxon:','', taxon_b.rstrip())) not in self.tax_ids ) ):
+                    # TODO remove these filters, or parameterize them
+
+                    # ###uncomment the following code block if you want to filter based on taxid
+                    # taxids = [9606,10090,10116,7227,7955,6239,8355]  #our favorite animals
+                    # taxids = [9606] #human only
+                    # retain only those orthologous relationships to genes in the specified taxids
+                    # using AND will get you only those associations where gene1 AND gene2 are in the taxid list (most-filter)
+                    # using OR will get you any associations where gene1 OR gene2 are in the taxid list (some-filter)
+                    if ((int(re.sub('NCBITaxon:', '', taxon_a.rstrip())) not in self.tax_ids) and
+                            (int(re.sub('NCBITaxon:', '', taxon_b.rstrip())) not in self.tax_ids)):
                         continue
                     else:
                         matchcounter += 1
-                        if (limit is not None and matchcounter > limit):
+                        if limit is not None and matchcounter > limit:
                             break
 
-                    ###end code block for filtering on taxon
+                    # ###end code block for filtering on taxon
 
-                    #fix the gene identifiers
-                    gene_a = re.sub('=',':',gene_a)
-                    gene_b = re.sub('=',':',gene_b)
+                    # fix the gene identifiers
+                    gene_a = re.sub('=', ':', gene_a)
+                    gene_b = re.sub('=', ':', gene_b)
 
-                    gene_a = self._clean_up_gene_id(gene_a,species_a)
-                    gene_b = self._clean_up_gene_id(gene_b,species_b)
+                    gene_a = self._clean_up_gene_id(gene_a, species_a)
+                    gene_b = self._clean_up_gene_id(gene_b, species_b)
 
-                    #a special case here; mostly some rat genes they use symbols instead of identifiers.  will skip
-                    if (re.match('Gene:',gene_a) or re.match('Gene:',gene_b)):
+                    # a special case here; mostly some rat genes they use symbols instead of identifiers.  will skip
+                    if re.match('Gene:', str(gene_a)) or re.match('Gene:', str(gene_b)):
                         continue
 
-                    rel=self._map_orthology_code_to_RO(orthology_class)
+                    rel = self._map_orthology_code_to_RO(orthology_class)
 
-                    evidence = 'ECO:0000080'   #phylogenetic evidence
+                    evidence = 'ECO:0000080'  # phylogenetic evidence
 
-                    #note that the panther_id references a group of orthologs, and is not 1:1 with the rest
-                    assoc_id = self.make_id(('').join((panther_id,species_a,gene_a,protein_a,species_b,gene_b,protein_b,orthology_class)))
+                    # note that the panther_id references a group of orthologs, and is not 1:1 with the rest
+                    assoc_id = self.make_id(''.join((panther_id, species_a, gene_a,
+                                                     protein_a, species_b, gene_b,
+                                                     protein_b, orthology_class)))
 
-                    #add the association and relevant nodes to graph
-                    assoc = OrthologyAssoc(assoc_id,gene_a,gene_b,None,evidence)
+                    # add the association and relevant nodes to graph
+                    assoc = OrthologyAssoc(assoc_id, gene_a, gene_b, None, evidence)
                     assoc.setRelationship(rel)
-                    assoc.loadAllProperties(g)    #FIXME inefficient
+                    assoc.loadAllProperties(g)  # FIXME inefficient
 
-                    #add genes to graph; assume labels will be taken care of elsewhere
-                    gu.addClassToGraph(g,gene_a,None)
-                    gu.addClassToGraph(g,gene_b,None)
+                    # add genes to graph; assume labels will be taken care of elsewhere
+                    gu.addClassToGraph(g, gene_a, None)
+                    gu.addClassToGraph(g, gene_b, None)
 
                     assoc.addAssociationToGraph(g)
 
-                    #note this is incomplete... it won't construct the full family hierarchy, just the top-grouping
-                    assoc.addGeneFamilyToGraph(self.graph,(':').join(('PANTHER',panther_id)))
+                    # note this is incomplete... it won't construct the full family hierarchy, just the top-grouping
+                    assoc.addGeneFamilyToGraph(self.graph, ':'.join(('PANTHER', panther_id)))
 
-                    if (not testMode) and (limit is not None and line_counter > limit):
+                    if not self.testMode and limit is not None and line_counter > limit:
                         break
 
-            print("INFO: finished processing",f)
+            logger.info("finished processing %s", f)
 
         return
 
-
-    def _map_taxon_abbr_to_id(self,ptax):
-        '''
+    def _map_taxon_abbr_to_id(self, ptax):
+        """
         Will map the panther-specific taxon abbreviations to NCBI taxon numbers
         :param ptax:
         :return: NCBITaxon id
-        '''
+        """
         taxid = None
         ptax_to_taxid_map = {
-            'HUMAN' : 9606,
-            'SCHPO' : 4896,
-            'ARATH' : 3702,
-            'MOUSE' : 10090,
-            'DANRE' : 7955,
-            'YEAST' : 4932,
-            'RAT' : 10116,
-            'CAEEL' : 6239,
-            'DICDI' : 44689,
-            'CHICK' : 9031,
-            'DROME' : 7227,
-            'PIG' : 9823,
-            'HORSE' : 9796,
-            'CANFA' : 9615,
-            'XENTR' : 8364,
-            'TAKRU' : 31033,
-            'MACMU' : 9544,
-            'ORNAN' : 9258,
-            'PANTR' : 9598,
-            'ANOCA' : 28377,
-            'MONDO' : 13616,
-            'BOVIN' : 9913
+            'HUMAN': 9606,
+            'SCHPO': 4896,
+            'ARATH': 3702,
+            'MOUSE': 10090,
+            'DANRE': 7955,
+            'YEAST': 4932,
+            'RAT': 10116,
+            'CAEEL': 6239,
+            'DICDI': 44689,
+            'CHICK': 9031,
+            'DROME': 7227,
+            'PIG': 9823,
+            'HORSE': 9796,
+            'CANFA': 9615,
+            'XENTR': 8364,
+            'TAKRU': 31033,
+            'MACMU': 9544,
+            'ORNAN': 9258,
+            'PANTR': 9598,
+            'ANOCA': 28377,
+            'MONDO': 13616,
+            'BOVIN': 9913
         }
-        if (ptax in ptax_to_taxid_map):
-            taxid = (':').join(('NCBITaxon',str(ptax_to_taxid_map.get(ptax))))
+        if ptax in ptax_to_taxid_map:
+            taxid = ':'.join(('NCBITaxon', str(ptax_to_taxid_map.get(ptax))))
         else:
-            print("ERROR: unmapped taxon code",ptax)
+            logger.error("unmapped taxon code %s", ptax)
 
         return taxid
 
-    def _map_orthology_code_to_RO(self,ortho):
-        '''
+    def _map_orthology_code_to_RO(self, ortho):
+        """
         Map the panther-specific orthology code (P,O,LDO,X,LDX) to relationship-ontology
         identifiers.
         :param ortho: orthology code
         :return: RO identifier
-        '''
-        o = OrthologyAssoc(None,None,None,None,None)  #this is sneaky, needs refactor
+        """
+        o = OrthologyAssoc(None, None, None, None, None)  # this is sneaky, needs refactor
 
-        ro_id = o.properties['orthologous'] #in orthology relationship with
+        ro_id = o.properties['orthologous']  # in orthology relationship with
         ortho_to_ro_map = {
-            'P' : o.properties['paralogous'],
-            'O' : o.properties['orthologous'],
+            'P': o.properties['paralogous'],
+            'O': o.properties['orthologous'],
             'LDO': o.properties['least_diverged_orthologous'],
             'X': o.properties['xenologous'],
             'LDX': o.properties['xenologous']
         }
 
-        if (ortho in ortho_to_ro_map):
+        if ortho in ortho_to_ro_map:
             ro_id = ortho_to_ro_map.get(ortho)
         else:
-            print("WARN: unmapped orthology code",ortho,". Defaulting to 'orthology'.")
+            logger.warn("unmapped orthology code %s. Defaulting to 'orthology'", ortho)
 
         return ro_id
 
-    def _clean_up_gene_id(self,id,sp):
-        '''
+    def _clean_up_gene_id(self, geneid, sp):
+        """
         A series of identifier rewriting to conform with standard gene identifiers.
-        :param id:
+        :param geneid:
         :param sp:
         :return:
-        '''
-        #special case for MGI
-        id = re.sub('MGI:MGI:','MGI:',id)
+        """
+        # special case for MGI
+        geneid = re.sub('MGI:MGI:', 'MGI:', geneid)
 
-        #rewrite Ensembl --> ENSEMBL
-        id = re.sub('Ensembl','ENSEMBL',id)
+        # rewrite Ensembl --> ENSEMBL
+        geneid = re.sub('Ensembl', 'ENSEMBL', geneid)
 
-        #rewrite Gene:CELE --> WormBase  these are old-school cosmid identifier
-        id = re.sub('Gene:CELE','WormBase:',id)
-        if (sp == 'CAEEL'):
-            if (re.match('(Gene|ENSEMBLGenome):\w+\\.\d+',id)):
-                id = re.sub('(?:Gene|ENSEMBLGenome):(\w+\\.\d+)','WormBase:\\1',id)
+        # rewrite Gene:CELE --> WormBase  these are old-school cosmid identifier
+        geneid = re.sub('Gene:CELE', 'WormBase:', geneid)
+        if sp == 'CAEEL':
+            if re.match('(Gene|ENSEMBLGenome):\w+\\.\d+', geneid):
+                geneid = re.sub('(?:Gene|ENSEMBLGenome):(\w+\\.\d+)', 'WormBase:\\1', geneid)
 
-        #rewrite GeneID --> NCBIGene
-        id = re.sub('GeneID','NCBIGene',id)
+        # rewrite GeneID --> NCBIGene
+        geneid = re.sub('GeneID', 'NCBIGene', geneid)
 
-        #rewrite Gene:Dmel --> FlyBase
-        id = re.sub('Gene:Dmel_','FlyBase:',id)
-        #rewrite Gene:CG --> FlyBase:CG
-        id = re.sub('Gene:CG','FlyBase:CG',id)
+        # rewrite Gene:Dmel --> FlyBase
+        geneid = re.sub('Gene:Dmel_', 'FlyBase:', geneid)
+        # rewrite Gene:CG --> FlyBase:CG
+        geneid = re.sub('Gene:CG', 'FlyBase:CG', geneid)
 
-        #rewrite ENSEMBLGenome:FBgn --> FlyBase:FBgn
-        id = re.sub('ENSEMBLGenome:FBgn','FlyBase:FBgn',id)
+        # rewrite ENSEMBLGenome:FBgn --> FlyBase:FBgn
+        geneid = re.sub('ENSEMBLGenome:FBgn', 'FlyBase:FBgn', geneid)
 
-        #rewrite Gene:<ensembl ids> --> ENSEMBL:<id>
-        id = re.sub('Gene:ENS','ENSEMBL:ENS',id)
+        # rewrite Gene:<ensembl ids> --> ENSEMBL:<id>
+        geneid = re.sub('Gene:ENS', 'ENSEMBL:ENS', geneid)
 
-        if (re.match('Gene:',id)):
-            print("WARN: Found something I don't know how to fix ( species",sp,"):",id)
+        if re.match('Gene:', geneid):
+            logger.warn("Found something I don't know how to fix (species %s): %s", sp, geneid)
 
-        return id
+        return geneid
+
+    def getTestSuite(self):
+        import unittest
+        from tests.test_panther import PantherTestCase
+
+        test_suite = unittest.TestLoader().loadTestsFromTestCase(PantherTestCase)
+
+        return test_suite
