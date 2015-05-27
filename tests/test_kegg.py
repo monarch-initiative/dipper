61c4e0a7321c746755fd3a743e08382924f56ed1
Nicole Washington <nicole.l.washington@gmail.com>
2015-05-27 12:50:30 -0700

    added tests for kegg

---
+++ tests/test_kegg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from dipper.sources.KEGG import KEGG
+from dipper import curie_map
+from rdflib import Graph
+from tests import test_general, test_source
+from tests.test_source import SourceTestCase
+
+import unittest
+import logging
+import os
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+class KEGGTestCase(SourceTestCase):
+
+    def setUp(self):
+        self.source = KEGG()
+        self.source.settestonly(True)
+        self._setDirToSource()
+        return
+
+    def tearDown(self):
+        self.source = None
+        return
+
+    # TODO add some specific tests to make sure we are hitting all parts of the code
+
+
+if __name__ == '__main__':
+    unittest.main()
