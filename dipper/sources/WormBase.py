71665bb88c20553c1120518f921bb2a029ac7612
Nicole Washington <nicole.l.washington@gmail.com>
2015-10-02 12:15:20 -0700

    add testing ids to WormBase

--- dipper/sources/WormBase.py
+++ dipper/sources/WormBase.py
@@ -76,8 +76,8 @@ class WormBase(Source):
     }
 
     test_ids = {
-        'gene': ['WBGene00001414', 'WBGene00004967', 'WBGene00003916', 'WBGene00004397'],
-        'allele': ['WBVar00087800', 'WBVar00087742', 'WBVar00144481', 'WBVar00248869'],
+        'gene': ['WBGene00001414', 'WBGene00004967', 'WBGene00003916', 'WBGene00004397', 'WBGene00001531'],
+        'allele': ['WBVar00087800', 'WBVar00087742', 'WBVar00144481', 'WBVar00248869', 'WBVar00250630'],
         'strain': ['BA794', 'RK1', 'HE1006'],
         'pub': []  # FIXME
     }
