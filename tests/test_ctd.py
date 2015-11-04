c316c2329e695f6f28c5922278029b1bd37a811d
Nicole Washington <nicole.l.washington@gmail.com>
2015-11-03 22:01:52 -0800

    ctd: fix failing test
    * since the assoc refactor, this test should have been failing.
    * this now uses the OBAN:association* types and properties in the sparql query
    * use correct rel_id (as specified)
    * alter logging in assertion

--- tests/test_ctd.py
+++ tests/test_ctd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from dipper.sources.CTD import CTD
+from dipper.models.assoc.G2PAssoc import G2PAssoc
 from tests.test_source import SourceTestCase
 
 import unittest
@@ -35,14 +36,12 @@ class CTDTestCase(SourceTestCase):
         # Expected structure
         # TODO can this be unified OBAN and the Annot models to be automatically generated?
         sparql_query = """
-                       SELECT ?assoc ?pubmed ?disease ?chemical
+                       SELECT ?assoc ?disease ?rel ?chemical
                        WHERE {
-                       ?assoc a Annotation: ;
-                           dc:evidence OBO:ECO_0000033 ;
-                           dc:source ?pubmed ;
-                           :hasObject ?disease ;
-                           :hasPredicate OBO:RO_0002606 ;
-                           :hasSubject ?chemical .}
+                           ?assoc a OBAN:association ;
+                           OBAN:association_has_object ?disease ;
+                           OBAN:association_has_object_property ?rel ;
+                           OBAN:association_has_subject ?chemical .}
                        """
 
         # SPARQL variables to check
@@ -53,31 +52,24 @@ class CTDTestCase(SourceTestCase):
         disease_uri = gu.getNode(disease_id)
         eco = 'ECO:0000033'
         rel_id = gu.object_properties['substance_that_treats']
+        rel_uri = gu.getNode(rel_id)
         pubmed_id = 'PMID:16785264'
         pubmed_uri = gu.getNode(pubmed_id)
 
-        # consider replacing with make_ctd_chem_disease_assoc_id()
-        assoc_id = self.source.make_association_id('ctd', chem_id, rel_id, disease_id, eco, pubmed_id)
+        assoc = G2PAssoc(self.source.name, chem_id, disease_id, rel_id)
+        assoc_id = assoc.make_g2p_id()
         assoc_uri = gu.getNode(assoc_id)
 
         # One of the expected outputs from query
-        expected_output = [assoc_uri, pubmed_uri, disease_uri, chem_uri]
+        expected_output = [assoc_uri, disease_uri, rel_uri, chem_uri]
 
         # Query graph
         sparql_output = test_query.query_graph(sparql_query)
 
-        self.assertTrue(expected_output in sparql_output, "did not find expected association: " + assoc_id +
-                        " found: " + pprint.pformat(sparql_output))
+        self.assertTrue(expected_output in sparql_output, "did not find expected association: " + str(expected_output) +
+                        " found " + str(len(sparql_output)) + " others:\n"+str(sparql_output))
 
         logger.info("Test query data finished.")
-
-    # OTHER CTD-SPECIFIC TESTS GO HERE
-    # @unittest.skip('CTD-specific tests not yet defined')
-    # def test_ctd(self):
-    #    logger.info("A CTD-specific test")
-    #
-    #    return
-
 
 if __name__ == '__main__':
     unittest.main()
