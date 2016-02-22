aefbd0522bf55251d253d1b705c546c140ddd8e6
TomConlin <tomc@cs.uoregon.edu>
2016-02-22 01:44:15 -0800

    first pylint refactor

--- dipper/utils/CurieUtil.py
+++ dipper/utils/CurieUtil.py
@@ -5,12 +5,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class CurieUtil:
-
-    # curie_map should be in the form URI -> prefix:
-    # ie: 'http://foo.org/bar_' -> 'bar'
-    # this class will not currently handle multiple uris with different prefixes
+class CurieUtil(object):
+    '''
+    Create compact URI
+    '''
     def __init__(self, curie_map):
+        '''
+        curie_map should be in the form URI -> prefix:
+        ie: 'http://foo.org/bar_' -> 'bar'
+        this class will not currently handle
+        multiple URIs with different prefixes
+        '''
         self.curie_map = curie_map
         self.uri_map = {}
         if curie_map is not None:
@@ -18,8 +23,8 @@ class CurieUtil:
                 self.uri_map[value] = key
         return
 
-    # Get a CURIE from a uri
     def get_curie(self, uri):
+        '''Get a CURIE from a URI '''
         prefix = self.get_curie_prefix(uri)
         if prefix is not None:
             key = self.curie_map[prefix]
@@ -27,13 +32,14 @@ class CurieUtil:
         return None
 
     def get_curie_prefix(self, uri):
+        ''' Return the CURIE's prefix:'''
         for key, value in self.uri_map.items():
             if uri.startswith(key):
                 return value
         return None
 
-    # Get a URI from a CURIE
     def get_uri(self, curie):
+        ''' Get a URI from a CURIE '''
         if curie is None:
             return None
         parts = curie.split(':')
