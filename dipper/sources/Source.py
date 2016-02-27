7006a3da7850983b892bc5309b2c3907b42ef21f
TomConlin <tomc@cs.uoregon.edu>
2016-02-26 16:53:14 -0800

    pylint refactor

--- dipper/sources/Source.py
+++ dipper/sources/Source.py
@@ -1,18 +1,18 @@
 
-__author__ = 'nicole'
-
+import re
+import hashlib
+import urllib
+import os
+import time
+import logging
+from datetime import datetime
+from stat import ST_CTIME, ST_SIZE
 from rdflib import ConjunctiveGraph, Namespace
 from rdflib.namespace import FOAF, DC, RDFS, OWL
-
-import urllib, os, time, logging
-from urllib import request
-from datetime import datetime
-from stat import *
-import hashlib
 from dipper import curie_map
-import re
-
 from dipper.utils.GraphUtils import GraphUtils
+
+__author__ = 'nicole'
 
 logger = logging.getLogger(__name__)
 core_bindings = {'dc': DC, 'foaf': FOAF, 'rdfs': RDFS}
@@ -36,18 +36,22 @@ class Source:
         self.name = name
         self.path = ""
         self.graph = ConjunctiveGraph()
-        self.testgraph = ConjunctiveGraph()  # to be used to store a subset of data for testing downstream.
+        # to be used to store a subset of data for testing downstream.
+        self.testgraph = ConjunctiveGraph()
         self.triple_count = 0
         self.outdir = 'out'
         self.testdir = 'tests'
         self.rawdir = 'raw'
-        self.nobnodes = False  # set to True if you want to materialze identifiers for BNodes
+        self.dataset = None
+        # set to True if you want to materialze identifiers for BNodes
+        self.nobnodes = False
         if self.name is not None:
             self.rawdir = '/'.join((self.rawdir, self.name))
             self.outfile = '/'.join((self.outdir, self.name + ".ttl"))
             logger.info("Setting outfile to %s", self.outfile)
 
-            self.datasetfile = '/'.join((self.outdir, self.name + '_dataset.ttl'))
+            self.datasetfile = '/'.join((self.outdir,
+                                         self.name + '_dataset.ttl'))
             logger.info("Setting dataset file to %s", self.datasetfile)
 
             self.testfile = '/'.join((self.outdir, self.name + "_test.ttl"))
@@ -65,7 +69,8 @@ class Source:
             p = os.path.abspath(self.outdir)
             logger.info("created output directory %s", p)
 
-        # will be set to True if the intention is to only process and write the test data
+        # will be set to True if the intention is
+        # to only process and write the test data
         self.testOnly = False
         self.testMode = False
 
@@ -102,27 +107,31 @@ class Source:
         abstract method to fetch all data from an external resource.
         this should be overridden by subclasses
         :return: None
+
         """
         return
 
     def parse(self):
         """
-        abstract method to parse all data from an external resource, that was fetched in
-        fetch()
-        this should be overridden by subclasses
+        abstract method to parse all data from an external resource,
+        that was fetched in fetch() this should be overridden by subclasses
         :return: None
+
         """
         return
 
     def write(self, format='rdfxml', stream=None):
         """
-        This convenience method will write out all of the graphs associated with the source.
+        This convenience method will write out all of the graphs
+        associated with the source.
         Right now these are hardcoded to be a single "graph" and a "dataset".
-        If you do not supply stream='stdout' it will default write these to files.
+        If you do not supply stream='stdout'
+        it will default write these to files.
 
-        In addition, if the version number isn't yet set in the dataset, it will be set to the
-        date on file.
+        In addition, if the version number isn't yet set in the dataset,
+        it will be set to the date on file.
         :return: None
+
         """
         format_to_xtn = {
             'rdfxml': 'xml', 'turtle': 'ttl'
@@ -144,10 +153,11 @@ class Source:
                 datasetfile = '.'.join((datasetfile, format))
 
             logger.info("No version set for this datasource; setting to date issued.")
+
             if self.dataset is not None and self.dataset.version is None:
                 self.dataset.set_version_by_date()
         else:
-            logger.warn("No output file set. Using stdout")
+            logger.warning("No output file set. Using stdout")
             stream = 'stdout'
 
         # start off with only the dataset descriptions
@@ -168,7 +178,7 @@ class Source:
             f = None
             if stream is None:
                 f = g['file']
-            elif stream.lowercase().strip() == 'stdout':
+            elif stream.lower().strip() == 'stdout':
                 f = None
             else:
                 logger.error("I don't understand your stream.")
@@ -201,9 +211,9 @@ class Source:
 
     def checkIfRemoteIsNewer(self, remote, local, headers):
         """
-        Given a remote file location, and the corresponding local file
-        this will check the datetime stamp on the files to see if the remote one
-        is newer.  This is a convenience method to be used so that we don't have to
+        Given a remote file location, and the corresponding local file this will
+        check the datetime stamp on the files to see if the remote one is newer.
+        This is a convenience method to be used so that we don't have to
         re-fetch files that we already have saved locally
         :param remote: URL of file to fetch from remote server
         :param local: pathname to save file to locally
@@ -229,17 +239,20 @@ class Source:
             resp_header = response.getheaders()
             size = resp_header.get('Content-length')
             last_modified = resp_header.get('last-modified')  # check me
-        except:
+        except OSError as e:  #URLError?
             resp_header = None
             size = 0
             last_modified = None
+            logger.error(e)
 
         st = os.stat(local)
-        logger.info("Local file date: %s", datetime.utcfromtimestamp(st[ST_CTIME]))
+        logger.info("Local file date: %s",
+                    datetime.utcfromtimestamp(st[ST_CTIME]))
 
         if last_modified is not None:
             # Thu, 07 Aug 2008 16:20:19 GMT
-            dt_obj = datetime.strptime(last_modified, "%a, %d %b %Y %H:%M:%S %Z")
+            dt_obj = datetime.strptime(last_modified,
+                                       "%a, %d %b %Y %H:%M:%S %Z")
             # get local file details
 
             # check date on local vs remote file
@@ -277,7 +290,8 @@ class Source:
 
         filedate = datetime.utcfromtimestamp(st[ST_CTIME]).strftime("%Y-%m-%d")
 
-        # FIXME change this so the date is attached only to each file, not the entire dataset
+        # FIXME change this so the date is attached only to each file,
+        # not the entire dataset
         self.dataset.set_date_issued(filedate)
 
         return
@@ -285,16 +299,18 @@ class Source:
     def fetch_from_url(self, remotefile, localfile, is_dl_forced, headers=None):
         """
         Given a remote url and a local filename, this will first verify
-        if the remote file is newer; if it is, this will pull the remote file
-        and save it to the specified localfile, reporting the basic file information
-        once it is downloaded
+        if the remote file is newer; if it is,
+        this will pull the remote file and save it to the specified localfile,
+        reporting the basic file information once it is downloaded
         :param remotefile: URL of remote file to fetch
         :param localfile: pathname of file to save locally
         :return: None
+
         """
+
         CHUNK = 16 * 1024
         if ((is_dl_forced is True) or
-           (self.checkIfRemoteIsNewer(remotefile, localfile, headers))):
+                (self.checkIfRemoteIsNewer(remotefile, localfile, headers))):
             logger.info("Fetching from %s", remotefile)
             # TODO url verification, etc
             if headers is not None:
@@ -305,10 +321,11 @@ class Source:
             response = urllib.request.urlopen(r)
 
             with open(localfile, 'wb') as fd:
-               while True:
-                  chunk = response.read(CHUNK)
-                  if not chunk: break
-                  fd.write(chunk)
+                while True:
+                    chunk = response.read(CHUNK)
+                    if not chunk:
+                        break
+                    fd.write(chunk)
 
             logger.info("Finished.  Wrote file to %s", localfile)
             if self.compare_local_remote_bytes(remotefile, localfile):
@@ -322,22 +339,27 @@ class Source:
 
         st = os.stat(localfile)
         logger.info("file size: %s", st[ST_SIZE])
-        logger.info("file created: %s", time.asctime(time.localtime(st[ST_CTIME])))
+        logger.info("file created: %s",
+                    time.asctime(time.localtime(st[ST_CTIME])))
         return
 
     def process_xml_table(self, elem, table_name, processing_function, limit):
         """
-        This is a convenience function to process the elements of an xml document, when the xml is used
-        as an alternative way of distributing sql-like tables.  In this case, the "elem" is akin to an
-        sql table, with it's name of ```table_name```.  It will then process each ```row``` given the
-        ```processing_function``` supplied.
+        This is a convenience function to process the elements of
+        an xml document, when the xml is used as an alternative way
+        of distributing sql-like tables.  In this case, the "elem" is akin to an
+        sql table, with it's name of ```table_name```.
+        It will then process each ```row```
+            given the ```processing_function``` supplied.
 
         :param elem: The element data
         :param table_name: The name of the table to process
         :param processing_function: The row processing function
         :param limit:
         :return:
+
         """
+
         line_counter = 0
         table_data = elem.find("[@name='"+table_name+"']")
         if table_data is not None:
@@ -399,8 +421,9 @@ class Source:
 
             resp_header = response.getheaders()
             byte_size = resp_header.get('Content-length')
-        except :
+        except OSError as e:
             byte_size = None
+            logger.error(e)
 
         return byte_size
 
@@ -425,7 +448,7 @@ class Source:
             is_equal = False
             logger.error('local file and remote file different sizes\n'
                          '%s has size %s, %s has size %s', localfile,
-                          local_size, remotefile, remote_size)
+                         local_size, remotefile, remote_size)
         return is_equal
 
     def file_len(self, fname):
@@ -446,7 +469,9 @@ class Source:
 
     def settestmode(self, mode):
         """
-        Set testMode to (mode).  True: run the Source in testMode; False: run it in full mode
+        Set testMode to (mode).
+            True: run the Source in testMode;
+            False: run it in full mode
         :param mode:
         :return: None
         """
@@ -457,17 +482,21 @@ class Source:
 
     def getTestSuite(self):
         """
-        An abstract method that should be overwritten with tests appropriate for the specific source.
+        An abstract method that should be overwritten with
+        tests appropriate for the specific source.
         :return:
         """
         return None
 
     def setnobnodes(self, materialize_bnodes):
         """
-        If materialze_bnodes is True, then all usages of BNodes will be materialized by putting the BNodes
-        into the BASE space, and prefixing the numeric portion (after the colon) with an underscore.
+        If materialze_bnodes is True,
+        then all usages of BNodes will be materialized
+        by putting the BNodes into the BASE space,
+        and prefixing the numeric portion (after the colon) with an underscore.
         :param materialize_bnodes:
         :return:
+
         """
 
         self.nobnodes = materialize_bnodes
@@ -476,7 +505,8 @@ class Source:
 
     def declareAsOntology(self, graph):
         """
-        The file we output needs to be declared as an ontology, including it's version information.
+        The file we output needs to be declared as an ontology,
+        including it's version information.
         Further information will be augmented in the dataset object.
         :param version:
         :return:
@@ -506,7 +536,8 @@ class Source:
     def remove_backslash_r(filename, encoding):
         """
         A helpful utility to remove '\r' from any file.
-        This will read a file into memory, and overwrite the contents of the original file.
+        This will read a file into memory,
+        and overwrite the contents of the original file.
         :param filename:
         :return:
         """
