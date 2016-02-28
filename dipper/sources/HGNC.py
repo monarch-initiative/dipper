cdbaf342d5ed3f8ec64b0d676d3960421e760b1c
TomConlin <tomc@cs.uoregon.edu>
2016-02-28 11:57:13 -0800

    pylint refactor

--- dipper/sources/HGNC.py
+++ dipper/sources/HGNC.py
@@ -24,8 +24,9 @@ class HGNC(Source):
     """
 
     files = {
-        'genes': {'file': 'hgnc_complete_set.txt',
-                  'url': 'ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt'},
+        'genes': {
+            'file': 'hgnc_complete_set.txt',
+            'url': 'ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt'},
     }
 
     def __init__(self, tax_ids=None, gene_ids=None):
@@ -39,7 +40,7 @@ class HGNC(Source):
 
         self.gene_ids = []
         if 'test_ids' not in config.get_config() or 'gene' not in config.get_config()['test_ids']:
-            logger.warn("not configured with gene test ids.")
+            logger.warning("not configured with gene test ids.")
         else:
             self.gene_ids = config.get_config()['test_ids']['gene']
 
@@ -91,14 +92,16 @@ class HGNC(Source):
         with open(raw, 'r', encoding="utf8") as csvfile:
             filereader = csv.reader(csvfile, delimiter='\t', quotechar='\"')
             for row in filereader:
-                (hgnc_id, symbol, name, locus_group, locus_type, status, location, location_sortable,
-                 alias_symbol, alias_name, prev_symbol, prev_name, gene_family, gene_family_id,
-                 date_approved_reserved, date_symbol_changed, date_name_changed, date_modified,
-                 entrez_id, ensembl_gene_id, vega_id, ucsc_id, ena, refseq_accession, ccds_id,
-                 uniprot_ids, pubmed_id, mgd_id, rgd_id, lsdb, cosmic, omim_id, mirbase, homeodb,
-                 snornabase, bioparadigms_slc, orphanet, pseudogene_org, horde_id, merops, imgt,
-                 iuphar, kznf_gene_catalog, mamit_trnadb, cd, lncrnadb, enzyme_id,
-                 intermediate_filament_db) = row
+                (hgnc_id, symbol, name, locus_group, locus_type, status,
+                 location, location_sortable, alias_symbol, alias_name,
+                 prev_symbol, prev_name, gene_family, gene_family_id,
+                 date_approved_reserved, date_symbol_changed, date_name_changed,
+                 date_modified, entrez_id, ensembl_gene_id, vega_id, ucsc_id,
+                 ena, refseq_accession, ccds_id, uniprot_ids, pubmed_id, mgd_id,
+                 rgd_id, lsdb, cosmic, omim_id, mirbase, homeodb, snornabase,
+                 bioparadigms_slc, orphanet, pseudogene_org, horde_id, merops,
+                 imgt, iuphar, kznf_gene_catalog, mamit_trnadb, cd, lncrnadb,
+                 enzyme_id, intermediate_filament_db) = row
 
                 line_counter += 1
 
@@ -118,34 +121,40 @@ class HGNC(Source):
                 if entrez_id != '':
                     gu.addEquivalentClass(g, hgnc_id, 'NCBIGene:' + entrez_id)
                 if ensembl_gene_id != '':
-                    gu.addEquivalentClass(g, hgnc_id, 'ENSEMBL:' + ensembl_gene_id)
+                    gu.addEquivalentClass(g, hgnc_id,
+                                          'ENSEMBL:' + ensembl_gene_id)
                 geno.addTaxon('NCBITaxon:9606', hgnc_id)
 
                 # add pubs as "is about"
                 if pubmed_id != '':
-                    for p in re.split('\|', pubmed_id.strip()):
+                    for p in re.split(r'\|', pubmed_id.strip()):
                         if str(p) != '':
-                            gu.addTriple(g, 'PMID:' + str(p.strip()), gu.object_properties['is_about'], hgnc_id)
+                            gu.addTriple(g, 'PMID:' + str(p.strip()),
+                                         gu.object_properties['is_about'],
+                                         hgnc_id)
 
                 # add chr location
-                # sometimes two are listed, like: 10p11.2 or 17q25  -- there are only 2 of these FRA10A and MPFD
+                # sometimes two are listed, like: 10p11.2 or 17q25
+                # -- there are only 2 of these FRA10A and MPFD
                 # sometimes listed like "1 not on reference assembly"
                 # sometimes listed like 10q24.1-q24.3
                 # sometimes like 11q11 alternate reference locus
                 band = chrom = None
-                chr_pattern = '(\d+|X|Y|Z|W|MT)[pq$]'
+                chr_pattern = r'(\d+|X|Y|Z|W|MT)[pq$]'
                 chr_match = re.match(chr_pattern, location)
                 if chr_match is not None and len(chr_match.groups()) > 0:
                     chrom = chr_match.group(1)
                     chrom_id = makeChromID(chrom, 'NCBITaxon:9606', 'CHR')
-                    band_pattern = '([pq][A-H\d]?\d?(?:\.\d+)?)'
+                    band_pattern = r'([pq][A-H\d]?\d?(?:\.\d+)?)'
                     band_match = re.search(band_pattern, location)
                     f = Feature(hgnc_id, None, None)
                     if band_match is not None and len(band_match.groups()) > 0:
                         band = band_match.group(1)
                         band = chrom + band
-                        # add the chr band as the parent to this gene as a feature
-                        # but assume that the band is created as a class with properties elsewhere in Monochrom
+                        # add the chr band as the parent to this gene
+                        # as a feature but assume that the band is created
+                        # as a class with properties elsewhere in Monochrom
+                        # TEC Monoch? Monarchdom??
                         band_id = makeChromID(band, 'NCBITaxon:9606', 'CHR')
                         gu.addClassToGraph(g, band_id, None)
                         f.addSubsequenceOfFeature(g, band_id)
@@ -167,8 +176,10 @@ class HGNC(Source):
 
     def get_symbol_id_map(self):
         """
-        A convenience method to create a mapping between the HGNC symbols and their identifiers.
+        A convenience method to create a mapping between the HGNC
+        symbols and their identifiers.
         :return:
+
         """
 
         symbol_id_map = {}
@@ -176,14 +187,16 @@ class HGNC(Source):
         with open(f, 'r', encoding="utf8") as csvfile:
             filereader = csv.reader(csvfile, delimiter='\t', quotechar='\"')
             for row in filereader:
-                (hgnc_id, symbol, name, locus_group, locus_type, status, location, location_sortable,
-                 alias_symbol, alias_name, prev_symbol, prev_name, gene_family, gene_family_id,
-                 date_approved_reserved, date_symbol_changed, date_name_changed, date_modified,
-                 entrez_id, ensembl_gene_id, vega_id, ucsc_id, ena, refseq_accession, ccds_id,
-                 uniprot_ids, pubmed_id, mgd_id, rgd_id, lsdb, cosmic, omim_id, mirbase, homeodb,
-                 snornabase, bioparadigms_slc, orphanet, pseudogene_org, horde_id, merops, imgt,
-                 iuphar, kznf_gene_catalog, mamit_trnadb, cd, lncrnadb, enzyme_id,
-                 intermediate_filament_db) = row
+                (hgnc_id, symbol, name, locus_group, locus_type, status,
+                 location, location_sortable, alias_symbol, alias_name,
+                 prev_symbol, prev_name, gene_family, gene_family_id,
+                 date_approved_reserved, date_symbol_changed, date_name_changed,
+                 date_modified, entrez_id, ensembl_gene_id, vega_id, ucsc_id,
+                 ena, refseq_accession, ccds_id, uniprot_ids, pubmed_id, mgd_id,
+                 rgd_id, lsdb, cosmic, omim_id, mirbase, homeodb, snornabase,
+                 bioparadigms_slc, orphanet, pseudogene_org, horde_id, merops,
+                 imgt, iuphar, kznf_gene_catalog, mamit_trnadb, cd, lncrnadb,
+                 enzyme_id, intermediate_filament_db) = row
 
                 symbol_id_map[symbol.strip()] = hgnc_id
 
@@ -192,31 +205,37 @@ class HGNC(Source):
     @staticmethod
     def _get_gene_type(locus_type):
         """
-        Given the locus_type string supplied by HGNC, we map them to relevant SO terms.  We use the
-        mappings listed in the "?" popup on any HGNC gene page.
+        Given the locus_type string supplied by HGNC,
+        we map them to relevant SO terms.
+        We use the mappings listed in the "?" popup on any HGNC gene page.
         :param locus_type:
         :return:
+
         """
 
         locus_type_map = {
             'RNA, Y': 'SO:0000405',
-            'RNA, cluster': 'SO:0000655',  # ??? ncRNA
-            'RNA, long non-coding': 'SO:0001877',  # lncRNA
-            'RNA, micro': 'SO:0001265',  # miRNA
-            'RNA, misc': 'SO:0000655',  # ??? ncRNA
-            'RNA, ribosomal': 'SO:0001637',  # rRNA
-            'RNA, small cytoplasmic': 'SO:0001266',  # small cytoplasmic is synonym of scRNA_primary_transcript
-            'RNA, small nuclear': 'SO:0001268',  # snRNA
-            'RNA, small nucleolar': 'SO:0001267',  # snoRNA
-            'RNA, transfer': 'SO:0001272',  # tRNA
-            'RNA, vault': 'SO:0000404',  # vault_RNA
-            'T cell receptor gene': 'SO:0000460',  # vertebrate_immunoglobulin_T_cell_receptor_segment
+            'RNA, cluster': 'SO:0000655',           # ??? ncRNA
+            'RNA, long non-coding': 'SO:0001877',   # lncRNA
+            'RNA, micro': 'SO:0001265',             # miRNA
+            'RNA, misc': 'SO:0000655',              # ??? ncRNA
+            'RNA, ribosomal': 'SO:0001637',         # rRNA
+            # small cytoplasmic is synonym of scRNA_primary_transcript
+            'RNA, small cytoplasmic': 'SO:0001266',
+            'RNA, small nucleolar': 'SO:0001267',   # snoRNA
+            'RNA, transfer': 'SO:0001272',          # tRNA
+            'RNA, vault': 'SO:0000404',             # vault_RNA
+            # vertebrate_immunoglobulin_T_cell_receptor_segment
+            'T cell receptor gene': 'SO:0000460',
             'T cell receptor pseudogene': 'SO:0000336',  # pseudogene
-            'complex locus constituent': 'SO:0001217',  # protein_coding - no way to say member of cluster
+            # protein_coding - no way to say member of cluster
+            'complex locus constituent': 'SO:0001217',
             'endogenous retrovirus': 'SO:0000100',  # endogenous_retroviral_gene
-            'fragile site': 'SO:0001411',  # biological_region TODO https://sourceforge.net/p/song/term-tracker/433/
+            # biological_region
+            # TODO https://sourceforge.net/p/song/term-tracker/433/
+            'fragile site': 'SO:0001411',
             'gene with protein product': 'SO:0001217',  # protein_coding_gene
-            'immunoglobulin gene': 'SO:0000460',  # immunoglobulin_region
+            'immunoglobulin gene': 'SO:0000460',        # immunoglobulin_region
             'immunoglobulin pseudogene': 'SO:0000336',  # pseudogene  FIXME
             'phenotype only': 'SO:0001500',  # heritable_phenotypic_marker
             'protocadherin': 'SO:0000110',  # FIXME
