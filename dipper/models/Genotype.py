989a85fe5d45aecaf227661225c6ba3727c2f16b
TomConlin <tomc@cs.uoregon.edu>
2017-02-11 12:45:24 -0800

    leave bnode as was

--- dipper/models/Genotype.py
+++ dipper/models/Genotype.py
@@ -482,9 +482,10 @@ class Genotype():
 
         # akin to a variant locus
         if targeted_gene_id is None:
-            targeted_gene_id = '_:' + gene_id + '-' + reagent_id
-        self.gu.addIndividualToGraph(
-            self.graph, targeted_gene_id, targeted_gene_label,
+            targeted_gene_id = '_' + gene_id + '-' + reagent_id
+            targeted_gene_id = targeted_gene_id.replace(":", "")
+        self.model.addIndividualToGraph(
+            targeted_gene_id, targeted_gene_label,
 
             self.genoparts['reagent_targeted_gene'], description)
 
