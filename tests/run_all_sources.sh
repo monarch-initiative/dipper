3349e484048337efb5e86e389dd85dad30735a80
TomConlin <tomc@cs.uoregon.edu>
2017-02-18 17:15:28 -0800

    ditch the hardcoded list, should do ths within dipper.py too

--- tests/run_all_sources.sh
+++ tests/run_all_sources.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-../dipper.py --sources hpoa,biogrid,impc,panther,ncbigene,ucscbands,ctd,genereviews,eom,zfin,clinvar,coriell,mgi --no_verify
+# ../dipper.py --sources hpoa,biogrid,impc,panther,ncbigene,\
+# ucscbands,ctd,genereviews,eom,zfin,clinvar,coriell,mgi
+
+# dont want the hardcoded sources
+
+# don't want the random __init__.py file
+for source in ../dipper/sources/[^_]*.py; do
+    file=${source##*/}
+    name=${file%.py}
+    src=${name,,}
+    # don't want the resident super classes: Sources & PostgresqlSource
+    if ! [[ ${src} =~ ".*sources$" ]]; then
+        # echo "../dipper.py --sources ${src}";  # --no_verify
+        ../dipper.py --sources ${src}"
+    fi
+done
