ccaf355894c1b07d8a13b89022c907061863a8ca
TomConlin <tomc@cs.uoregon.edu>
2017-02-23 10:54:34 -0800

    comments & catch postgresqlsource

--- tests/run_all_sources.sh
+++ tests/run_all_sources.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# don't want the hardcoded sources
 # ../dipper.py --sources hpoa,biogrid,impc,panther,ncbigene,\
 # ucscbands,ctd,genereviews,eom,zfin,clinvar,coriell,mgi
 
-# dont want the hardcoded sources
 
-# don't want the random __init__.py file
+# about 20 hours sequentially
+# might want to run them in parallel
+# (i.e. panther first, along with everything else on other cores )
+
+# note this places the 'out' dir in the directory it located in
+
+# don't want the __init__.py file
 for source in ../dipper/sources/[^_]*.py; do
     file=${source##*/}
     name=${file%.py}
     src=${name,,}
     # don't want the resident super classes: Sources & PostgresqlSource
-    if ! [[ ${src} =~ ".*sources$" ]]; then
+    if ! [[ ${src} =~ ".*sources*$" ]]; then
         # echo "../dipper.py --sources ${src}";  # --no_verify
         ../dipper.py --sources "${src}"
     fi
