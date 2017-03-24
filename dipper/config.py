c026ac6876d3ab46687135faa261e43469734960
TomConlin <tomc@cs.uoregon.edu>
2017-03-23 22:33:28 -0700

    make what 'config' is specific in comments

--- dipper/config.py
+++ dipper/config.py
@@ -12,16 +12,17 @@ conf = {}
 '''
     Load the configuration file 'conf.json', if it exists.
     it isn't always required, but may be for some sources.
+    conf.json may contain sensitive info and should not live in a public repo
 '''
 
 if os.path.exists(os.path.join(os.path.dirname(__file__), 'conf.json')):
     with open(
-        os.path.join(os.path.dirname(__file__),
-                     'conf.json')) as json_file:
+        os.path.join(
+            os.path.dirname(__file__), 'conf.json')) as json_file:
         conf = json.load(json_file)
-        logger.debug("Finished loading config")
+        logger.debug("Finished loading dipper/config.json")
 else:
-    logger.warning("'conf.json' not found in '%s'", os.path.dirname(__file__))
+    logger.warning("'dipper/conf.json' not found in '%s'", os.path.dirname(__file__))
     logger.warning("Sources that depend on 'conf.json' will fail")
 
 
