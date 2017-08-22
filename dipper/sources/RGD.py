ba29a9af7a0a3440e6c5653888334e2933b50495
Tim Putman <putmantime@gmail.com>
2017-08-22 15:53:52 -0700

    added date_created module to Provenance and used in RGD ingest script

--- dipper/sources/RGD.py
+++ dipper/sources/RGD.py
@@ -1,10 +1,10 @@
 from dipper.sources.Source import Source
 from dipper.models.assoc.Association import Assoc
-from dipper.models.Pathway import Pathway
+from dipper.models.Model import Model
+from dipper.models.Provenance import Provenance
 from dipper.models.Dataset import Dataset
 from ontobio.io.gafparser import GafParser
 from pprint import pprint
-
 import logging
 
 __author__ = 'timputman'
@@ -21,7 +21,17 @@ class RGD(Source):
     }
 
     map_files = {
-        'eco_map': 'http://purl.obolibrary.org/obo/eco/gaf-eco-mapping.txt',
+        'eco_map': {'IEA': 'ECO:0000501',
+                    'IAGP': 'ECO:0005613',
+                    'IDA': 'ECO:0000314',
+                    'IMP': 'ECO:0000315',
+                    'IED': 'ECO:0005611',
+                    'TAS': 'ECO:0000304',
+                    'QTM': 'ECO:0000061',
+                    'ISS': 'ECO:0000250',
+                    'NAS': 'ECO:0000303',
+                    'IPM': 'ECO:0005612',
+                    'IEP': 'ECO:0000270'}
     }
 
     def __init__(self, graph_type, are_bnodes_skolemized):
@@ -39,8 +49,7 @@ class RGD(Source):
         Returns:
             :return None
         """
-        print(self.rawdir)
-
+        self.get_files(is_dl_forced)
         return
 
     def parse(self, limit=None):
@@ -57,6 +66,48 @@ class RGD(Source):
         rgd_file = '/'.join((self.rawdir, self.files['rat_gene2mammalian_phenotype']['file']))
         p = GafParser()
         assocs = p.parse(open(rgd_file, "r"))
+
+        for i, assoc in enumerate(assocs):
+            if assoc['date'] is None:
+                print(assoc)
+            if assoc['relation']['id'] is None:
+                assoc['relation']['id'] = 'RO:0002200'
+            self.make_association(assoc)
+            if limit is not None and i > limit:
+                break
+
+        return
+
+    def make_association(self, record):
+        model = Model(self.graph)
+        provenance_model = Provenance(self.graph)
+        redate = record['date'].replace('-', '')
+        assertion_bnode = self.make_id("{0}{1}{2}".format(record['subject']['label'],
+                                                          record['subject']['id'],
+                                                          record['object']['id']
+                                                          ), '_')
+
+        provenance_model.add_date_created(prov_type=assertion_bnode, date=redate)
+
+        model.addIndividualToGraph(
+            assertion_bnode, None,
+            provenance_model.provenance_types['assertion'])
+
+        gene = record['subject']['id']
+        relation = record['relation']['id']
+        phenotype = record['object']['id']
+
+        g2p_assoc = Assoc(self.graph, self.name, sub=gene, obj=phenotype, pred=relation)
+        references = record['evidence']['has_supporting_reference']
+
+        if len(references) > 0:
+            g2p_assoc.add_source(identifier=references[0])
+        if len(references) > 1:
+            for ref in references[1:]:
+                model.addSameIndividual(sub=references[0], obj=ref)
+
+        g2p_assoc.add_evidence(RGD.map_files['eco_map'][record['evidence']['type']])
+        g2p_assoc.add_association_to_graph()
 
         return
 
