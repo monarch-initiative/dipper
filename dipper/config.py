71f4125503cd8555d43d3a8f4dbeaf23913f0eac
TomConlin <corporate.drone1339@gmail.com>
2018-10-21 10:11:19 -0700

    conf refactor

--- dipper/config.py
+++ dipper/config.py
@@ -1,4 +1,4 @@
-import json
+import yaml
 import os.path
 import logging
 
@@ -19,15 +19,15 @@ conf = {
     conf.json may contain sensitive info and should not live in a public repo
 '''
 
-if os.path.exists(os.path.join(os.path.dirname(__file__), 'conf.json')):
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'conf.yaml')):
     with open(
         os.path.join(
-            os.path.dirname(__file__), 'conf.json')) as json_file:
-        conf = json.load(json_file)
-        logger.debug("Finished loading dipper/conf.json")
+            os.path.dirname(__file__), 'conf.yaml')) as yaml_file:
+        conf = yaml.safe_load(yaml_file)
+        logger.debug("Finished loading dipper/conf.yaml")
 else:
-    logger.warning("'dipper/conf.json' not found in '%s'", os.path.dirname(__file__))
-    logger.warning("Sources that depend on 'conf.json' will fail")
+    logger.warning("'dipper/conf.yaml' not found in '%s'", os.path.dirname(__file__))
+    logger.warning("Sources that depend on 'conf.yaml' will fail")
 
 
 def get_config():
