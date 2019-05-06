5754d208aaf31e5751cd16f8518382eca0f9e92a
kshefchek <kshefchek@gmail.com>
2019-05-06 10:12:43 -0700

    set log level to warning

--- tests/test_clinvar.py
+++ tests/test_clinvar.py
@@ -9,7 +9,7 @@ from dipper.utils.TestUtils import TestUtils
 from dipper.sources.ClinVarXML_alpha import parse as clinvar_parse
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.WARNING)
 LOG = logging.getLogger(__name__)
 
 TEST_PATH = os.path.join(os.path.dirname(__file__), 'resources/clinvar')
