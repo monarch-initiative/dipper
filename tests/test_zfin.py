e7151fb99649d33982e9274a92a130307a37285d
Justin Reese <justaddcoffee+github@gmail.com>
2019-05-11 10:25:29 -0700

    added simple test for _load_zp_mappings, saved mapped_zpids during loading, updated name of mapping file to prevent confusing collision while testing

--- tests/test_zfin.py
+++ tests/test_zfin.py
@@ -36,5 +36,18 @@ class ZFINTestCase(SourceTestCase):
 
         return
 
+    def test_load_zp_mappings(self):
+        if self.source is not None:
+            try:
+                zp_map = self.source._load_zp_mappings("./tests/resources/zfin/zp-mapping-test.txt")
+                self.assertIsInstance(zp_map, dict, "_load_zp_mappings() didn't return dict!")
+                self.assertTrue(len(zp_map) == 1, "_load_zp_mappings() didn't return exactly one thing!")
+                self.assertDictEqual(zp_map, {'MONARCH:b311b709bf1cf852d770': {'zp_id': 'ZP:0002959', 'subterm1_id': 'ZFA:0009114', 'post_composed_relationship_id_1': 'BFO:0000050', 'superterm1_id': 'ZFA:0001056', 'quality_id': 'PATO:0001453', 'subterm2_id': 'GO:0005927', 'post_composed_relationship_id_2': 'BFO:0000050', 'superterm2_id': 'ZFA:0001056'}}, "_load_zp_mappings() didn't return what I expected!")
+            except Exception as e:
+                logger.error(e)
+                
+        return
+
+    
 if __name__ == '__main__':
     unittest.main()
