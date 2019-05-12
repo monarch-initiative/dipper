1bfb2db3758a74e1a5070baf329ab62fca767990
Justin Reese <justaddcoffee+github@gmail.com>
2019-05-12 10:19:25 -0700

    changed exception var name to be >3 characters

--- tests/test_zfin.py
+++ tests/test_zfin.py
@@ -43,8 +43,8 @@ class ZFINTestCase(SourceTestCase):
                 self.assertIsInstance(zp_map, dict, "_load_zp_mappings() didn't return dict!")
                 self.assertTrue(len(zp_map) == 1, "_load_zp_mappings() didn't return exactly one thing!")
                 self.assertDictEqual(zp_map, {'MONARCH:b311b709bf1cf852d770': {'zp_id': 'ZP:0002959', 'subterm1_id': 'ZFA:0009114', 'post_composed_relationship_id_1': 'BFO:0000050', 'superterm1_id': 'ZFA:0001056', 'quality_id': 'PATO:0001453', 'subterm2_id': 'GO:0005927', 'post_composed_relationship_id_2': 'BFO:0000050', 'superterm2_id': 'ZFA:0001056'}}, "_load_zp_mappings() didn't return what I expected!")
-            except Exception as e:
-                logger.error(e)
+            except Exception as t_except:
+                logger.error(t_except)
                 
         return
 
