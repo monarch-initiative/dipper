c58aa4f6b56957aa043cf4dd82bda59322480958
Justin Reese <justaddcoffee+github@gmail.com>
2019-07-17 15:00:44 -0700

    Added categories to RGD ingest

--- dipper/sources/RGD.py
+++ dipper/sources/RGD.py
@@ -5,6 +5,7 @@ from dipper.sources.Source import Source
 from dipper.models.assoc.Association import Assoc
 from dipper.models.Model import Model
 from dipper.models.Reference import Reference
+from dipper.models.BiolinkVocabulary import BioLinkVocabulary as blv
 
 __author__ = 'timputman'
 
@@ -87,7 +88,9 @@ class RGD(Source):
         phenotype = record['object']['id']
 
         # instantiate the association
-        g2p_assoc = Assoc(self.graph, self.name, sub=gene, obj=phenotype, pred=relation)
+        g2p_assoc = Assoc(self.graph, self.name, sub=gene, obj=phenotype, pred=relation,
+                          subject_category=blv.Gene.value,
+                          object_category=blv.PhenotypicFeature.value)
 
         # add the references
         references = record['evidence']['has_supporting_reference']
@@ -109,7 +112,9 @@ class RGD(Source):
             # This seems to be specific to this source and
             # there could be non-equivalent references in this list
             for ref in references[1:]:
-                model.addSameIndividual(sub=references[0], obj=ref)
+                model.addSameIndividual(sub=references[0], obj=ref,
+                                        subject_category=blv.Publication.value,
+                                        object_category=blv.Publication.value)
 
         # add the date created on
         g2p_assoc.add_date(date=record['date'])
