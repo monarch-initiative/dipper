16425bd6a267d6f9036da0e6533349e0d6e54c08
Justin Reese <justaddcoffee+github@gmail.com>
2019-09-11 11:56:41 -0700

    Fixed incorrect call to set_ingest_source() in Coriell.py

--- dipper/sources/Coriell.py
+++ dipper/sources/Coriell.py
@@ -220,7 +220,8 @@ class Coriell(Source):
                     filedate = datetime.utcfromtimestamp(
                         fstat[stat.ST_CTIME]).strftime("%Y-%m-%d")
 
-                self.dataset.set_ingest_source(remotef.filename, True)
+                self.dataset.set_ingest_source(remotef.filename,
+                                               is_object_literal=True)
                 self.dataset.set_ingest_source_file_version_date(remotef, filedate)
 
         return
