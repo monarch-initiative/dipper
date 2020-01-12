558be621cacea263409f151abb0848b7c930a436
TomConlin <corporate.drone1339@gmail.com>
2020-01-12 01:59:49 -0800

    reduce expected info in logs

--- dipper/sources/Orphanet.py
+++ dipper/sources/Orphanet.py
@@ -105,9 +105,9 @@ class Orphanet(Source):
                 model.addClassToGraph(disorder_id, disorder_label)
                 assoc_list = elem.find('DisorderGeneAssociationList')
                 expected_genes = assoc_list.get('count')
-                LOG.info(
-                    'Expecting %s genes associated with disorder %s.',
-                    expected_genes, disorder_id)
+                # LOG.info(  # too chatty in the logs
+                #    'Expecting %s genes associated with disorder %s.',
+                #    expected_genes, disorder_id)
                 processed_genes = 0
                 for assoc in assoc_list.findall('DisorderGeneAssociation'):
                     processed_genes += 1
