a162ce59fb6f1fc653295525199b501e79ae3f6c
Justin Reese <justaddcoffee+github@gmail.com>
2020-03-02 11:29:34 -0800

    Catch possible URLError in Source.py

--- dipper/sources/Source.py
+++ dipper/sources/Source.py
@@ -456,6 +456,9 @@ class Source:
                 # raise Exception(httpErr.read())
                 LOG.error('NETWORK issue %s\n\tFor: %s', httpErr.read(), remoteurl)
                 return False  # allows re try (e.g. not found in Cache)
+            except urllib.error.URLError as urlErr:
+                LOG.error('URLError %s\n\tFor: %s', urlErr.read(), remoteurl)
+                return False
             if localfile is not None:
                 with open(localfile, 'wb') as binwrite:
                     while True:
