c603b9be9ab6fc5e1a53a7efaa560ce40ab24e41
TomConlin <corporate.drone1339@gmail.com>
2020-03-19 13:45:14 -0700

    inital add, for post processing in bulk

--- scripts/mdma.sh
+++ scripts/mdma.sh
@@ -3,32 +3,39 @@
 # mdma.sh
 # Move Data to Monarch Archive
 # to be run (estaticly) by jenkins ci
-
-    YYYYMM = $(YYYYMM:-date +%Y%m)
-    MONARCHIVE = $(MONARCHIVE:-'monarch-archive:/var/www/data/$YYYYMM')
+    DTSTMP=$(date +%Y%m%d)
+    YYYYMM="${YYYYMM:-$(date +%Y%m)}"
+    MONARCHIVE="monarch@monarch-archive:/var/www/data/$YYYYMM"
 
     # copy main turtle files to ntriple format
     # https://github.com/drobilla/serd
-    mkdir ntriples
-    mv out/*nt ntriples
+    mkdir -p ntriples
+    if [ -e  out/*.nt ] ; then
+        mv -u out/*.nt ntriples
+    fi
+
+    # enable extended gobbing for selecting filenames without an underbar
+    shopt -s extglob
     stat --printf="%s\t%n\n" ./out/+([^_]).ttl |sort -nr |cut -f2 |
-    parallel -j0 "serdi -i turtle -o ntriples {} > ntriples/{/.}.nt"
+        parallel -j0 "serdi -i turtle -o ntriples {} > ntriples/{/.}.nt"
 
     # compress the ntriples as machine readable shouldn't care (humans use zcat)
     tar -czf ntriples_$YYYYMM.tgz ntriples/
     # rename; as 'out' makes no sense downstream. todo: change in dipper
-    mv out rdf
-    mv ntriples_$YYYYMM.tgz rdf/
+    mv -u  out rdf
+    mv -u ntriples_$YYYYMM.tgz rdf/
     rm -fr ntriples/
     tar -czf rdf_$YYYYMM.tgz rdf
-    # move the dir back for now
-    mv rdf out
+
     # move the yamal files along with the data they were applied to
     scp dipper/curie_map.yaml $MONARCHIVE/translationtable/
-    scp translation_table/GLOBAL_terms.yaml $MONARCHIVE/translationtable/
+    scp translationtable/GLOBAL_TERMS.yaml $MONARCHIVE/translationtable/
     scp rdf_$YYYYMM.tgz $MONARCHIVE/
-    scp -r rdf_$YYYYMM.tgz rdf/* $MONARCHIVE/rdf/
+    scp -r ./rdf/* $MONARCHIVE/rdf/
+    # move the dir back for now
+    mv -u rdf out
 
     # need a higher resolution than year-month in this case
-    git tag -a $(date +%Y%m%d) -m "release candidate $(date +%Y%m%d)"
-    git push origin --tags
+    git tag -a $DTSTMP -m "release candidate $DTSTMP"
+    git push origin --tag
+
