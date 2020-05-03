f533841fa1e7d5fb9a75409b4d558dc2688e34b8
TomConlin <corporate.drone1339@gmail.com>
2020-05-03 14:56:21 -0700

    rewritten

--- resources/mgi/SQL/monarch_all_allele_view.sql
+++ resources/mgi/SQL/monarch_all_allele_view.sql
@@ -1,41 +1,67 @@
 /*
 First derived from  View "mgd.all_allele_view"
 
- ~ 30 seconds
+ ~ 45 seconds
 */
 
- SELECT a._allele_key,
-    a._marker_key,
-    a._strain_key,
-    a._mode_key,
-    a._allele_type_key,
-    a._allele_status_key,
-    a._transmission_key,
-    a._collection_key,
-    a.symbol,
-    a.name,
-    a.iswildtype,
-    a.isextinct,
-    a.ismixed,   -- gender
-    a._refs_key,
-    a._markerallele_status_key,
-    m.symbol AS markersymbol,
-    t.term,
-    t.sequencenum AS statusnum,
-    s.strain,
-    t2.term AS collection,
-    t3.term AS markerallele_status,
-    r.numericpart AS jnum,
-    r.jnumid,
-    r.citation,
-    r.short_citation
-   FROM all_allele a
-     LEFT JOIN mrk_marker m ON a._marker_key = m._marker_key
-     LEFT JOIN bib_citation_cache r ON a._refs_key = r._refs_key
-     join prb_strain s on a._strain_key = s._strain_key
-     join voc_term t on a._allele_status_key = t._term_key 
-     join voc_term t2 on a._collection_key = t2._term_key 
-     join voc_term t3 on a._markerallele_status_key = t3._term_key
+ SELECT 
+ 	aav._allele_key,
+ 
+    --aav._marker_key, --nullable
+    aav.markersymbol,
+ 
+    --aav._strain_key,
+    aav.strain,
 
-  ;
+    --aav._mode_key,
+    tin.term as inheritance,
 
+    --aav._allele_type_key,
+    tal.term as allele_type,
+
+    --aav._allele_status_key,
+    tas.term as allele_status,
+
+    --aav._transmission_key,
+    tat.term as transmission,
+
+    --aav._collection_key,
+    aav.symbol,
+    aav.name,
+    aav.iswildtype,
+    --aav.isextinct,
+    aav.ismixed as mix_gender,
+    --aav._refs_key,
+
+    --aav._markerallele_status_key, --nullable
+    aav.term AS markerallele_status,
+
+    --aav._createdby_key,
+    --aav._modifiedby_key,
+    --aav._approvedby_key,
+    --aav.approval_date,
+    --aav.creation_date,
+    --aav.modification_date,
+    --m.symbol AS markersymbol,
+
+    aav.term as allele_status,
+    --t.sequencenum AS statusnum, -- for internal ordering 
+
+    --aav.collection,
+    --u1.login AS createdby,
+    --u2.login AS modifiedby,
+    --u3.login AS approvedby,
+ 
+    aav.jnum,
+    aav.jnumid,
+    aav.citation,
+    aav.short_citation
+
+FROM all_allele_view aav
+  join voc_term tin on aav._mode_key = tin._term_key 
+  join voc_term tal on aav._allele_type_key = tal._term_key 
+  join voc_term tas on aav._allele_status_key = tas._term_key 
+  join voc_term tat on aav._transmission_key = tat._term_key 
+where tas.term != 'deleted'
+
+;
