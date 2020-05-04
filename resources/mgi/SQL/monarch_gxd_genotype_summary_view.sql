f86c11e8742d8aaed5af573ae90f7c72df0ff5c6
TomConlin <corporate.drone1339@gmail.com>
2020-05-04 14:13:05 -0700

    rewrite

--- resources/mgi/SQL/monarch_gxd_genotype_summary_view.sql
+++ resources/mgi/SQL/monarch_gxd_genotype_summary_view.sql
@@ -1,73 +1,33 @@
 /*
 First derived from  View "mgd.gxd_genotype_summary_view"
 
+	~30 seconds
 */
- SELECT a._accession_key,
-    a.accid,
-    at.name || ':' || a._object_key as mgi_internal,
-    a.accid AS mgiid,
-    s.strain AS subtype,
-    (((s.strain || ' '::text) || a1.symbol) || ','::text) || a2.symbol AS description,
-    (a1.symbol || ','::text) || a2.symbol AS short_description,
-    l.name AS logicaldb
-   FROM acc_accession a
-     JOIN gxd_genotype g ON a._object_key = g._genotype_key
-     JOIN prb_strain s ON g._strain_key = s._strain_key
-     JOIN gxd_allelepair ap ON g._genotype_key = ap._genotype_key
-     JOIN all_allele a1 ON ap._allele_key_1 = a1._allele_key
-     JOIN all_allele a2 ON ap._allele_key_2 = a2._allele_key
-     JOIN acc_logicaldb l ON a._logicaldb_key = l._logicaldb_key
-     join acc_mgitype at on a._mgitype_key =  at._mgitype_key
-    WHERE at.name = 'Genotype'
-    AND a._logicaldb_key = 1 
-    AND a.prefixpart = 'MGI:'::text 
-    AND a.preferred = 1
-    AND a.private = 0
-    AND a._logicaldb_key = l._logicaldb_key
-UNION
- SELECT a._accession_key,
-    a.accid,
-    at.name || ':' || a._object_key,
-    a.accid AS mgiid,
-    s.strain AS subtype,
-    (s.strain || ' '::text) || a1.symbol AS description,
-    a1.symbol AS short_description,
-    l.name AS logicaldb
-   FROM acc_accession a
-     JOIN gxd_genotype g ON a._object_key = g._genotype_key
-     JOIN prb_strain s ON g._strain_key = s._strain_key
-     JOIN gxd_allelepair ap ON g._genotype_key = ap._genotype_key AND ap._allele_key_2 IS NULL
-     JOIN all_allele a1 ON ap._allele_key_1 = a1._allele_key
-     JOIN acc_logicaldb l ON a._logicaldb_key = l._logicaldb_key
-  	 join acc_mgitype at on a._mgitype_key =  at._mgitype_key
-  WHERE at.name = 'Genotype' 
-    AND a._logicaldb_key = 1 
-    AND a.prefixpart = 'MGI:'::text 
-    AND a.preferred = 1 
-    AND a._logicaldb_key = l._logicaldb_key
-    AND a.private = 0
-UNION
- SELECT a._accession_key,
-    a.accid,
-    at.name || ':' || a._object_key,
-    a.accid AS mgiid,
-    s.strain AS subtype,
-    s.strain AS description,
-    NULL::text AS short_description,
-    l.name AS logicaldb
-   FROM acc_accession a
-     JOIN gxd_genotype g ON a._object_key = g._genotype_key
-     JOIN prb_strain s ON g._strain_key = s._strain_key
-     JOIN acc_logicaldb l ON a._logicaldb_key = l._logicaldb_key
-     join acc_mgitype at on a._mgitype_key =  at._mgitype_key
-  WHERE at.name = 'Genotype'
-    AND a._logicaldb_key = 1 
-    AND a.prefixpart = 'MGI:'::text 
-    AND a.preferred = 1 
-    AND a._logicaldb_key = l._logicaldb_key 
-    AND a.private = 0
-    AND NOT (EXISTS ( 
-        SELECT 1
-        FROM gxd_allelepair ap
-        WHERE g._genotype_key = ap._genotype_key))
+
+ SELECT
+	ggsv._accession_key,
+    ggsv.accid,
+    ggsv.prefixpart,
+    ggsv.numericpart,
+    --a._logicaldb_key,
+    ggsv._object_key as mgi_internal,
+    --a._mgitype_key,
+    at.name as mgi_type,
+
+    --a.private,
+    --a.preferred,
+    --a._createdby_key,
+    --a._modifiedby_key,
+    --a.creation_date,
+    --a.modification_date,
+    ggsv.mgiid,
+    ggsv.subtype,   -- strain
+    ggsv.description,
+    ggsv.short_description,
+    ggsv.logicaldb
+
+FROM gxd_genotype_summary_view ggsv
+join acc_mgitype at on ggsv._mgitype_key = at._mgitype_key
+WHERE ggsv.private !=1
+  AND ggsv.preferred = 1
 ;
