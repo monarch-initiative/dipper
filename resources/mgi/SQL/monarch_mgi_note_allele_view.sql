8db4b455a5f5ece8b82ae6641ca79065e9cc5a91
TomConlin <corporate.drone1339@gmail.com>
2020-05-04 14:35:10 -0700

    rewrite

--- resources/mgi/SQL/monarch_mgi_note_allele_view.sql
+++ resources/mgi/SQL/monarch_mgi_note_allele_view.sql
@@ -1,21 +1,22 @@
 /*
 First derived from View "mgd.mgi_note_allele_view"
- ~ 35 seconds
+ ~ 2 ninutes
 */
 
- SELECT n._note_key,
-    at.name || ':' || n._object_key as mgi_internal,
-    t.notetype,
-    c.note,
-    c.sequencenum,
-    m.name AS mgitype
-
-   FROM mgi_note n
-    join mgi_notetype t on n._notetype_key = t._notetype_key 
-    join mgi_notechunk c on n._note_key = c._note_key 
-    join acc_mgitype m on n._mgitype_key = m._mgitype_key 
-    join acc_mgitype at on t._mgitype_key =  at._mgitype_key
-  WHERE at.name = 'Allele'
-    AND t.private != 1
-
- ;
+ SELECT
+	nav._note_key,
+    nav._object_key as mgi_internal,
+    --n._mgitype_key,
+    --n._notetype_key,
+    --n._createdby_key,
+    --n._modifiedby_key,
+    --n.creation_date,
+    --n.modification_date,
+    nav.notetype,
+    nav.note,
+    --c.sequencenum,
+    nav.mgitype
+    --u1.login AS createdby,
+    --u2.login AS modifiedby
+FROM mgi_note_allele_view nav
+;
