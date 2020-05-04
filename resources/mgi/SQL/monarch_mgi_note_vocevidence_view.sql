00408bf431d6cb092149e6e9c2e16a4ab6240ba6
TomConlin <corporate.drone1339@gmail.com>
2020-05-04 14:45:48 -0700

    rewrite

--- resources/mgi/SQL/monarch_mgi_note_vocevidence_view.sql
+++ resources/mgi/SQL/monarch_mgi_note_vocevidence_view.sql
@@ -1,22 +1,24 @@
 /*
  First derived from View "mgd.mgi_note_vocevidence_view"
 
- ~ 20 seconds
+ ~ 30 seconds
 */
 
- SELECT n._note_key,
-    'AnnotationEvidence:' || n._object_key as mgi_internal,
-    n._mgitype_key,
-    n._notetype_key,
-    t.notetype,
-    c.note,
-    c.sequencenum,
-    at.name AS mgitype
-   FROM mgi_note n
-    join mgi_notetype t on n._notetype_key = t._notetype_key 
-    join mgi_notechunk c on n._note_key = c._note_key 
-    join acc_mgitype at on  n._mgitype_key = at._mgitype_key 
+ SELECT
+	nvv._note_key,
+    nvv._object_key as mgi_internal,
+    --n._mgitype_key,
+    --n._notetype_key,
+    --n._createdby_key,
+    --n._modifiedby_key,
+    --n.creation_date,
+    --n.modification_date,
+    nvv.notetype,
+    nvv.note,
+    --c.sequencenum,
+    nvv.mgitype
+    --u1.login AS createdby,
+    --u2.login AS modifiedby
 
-  WHERE at.name = 'Annotation Evidence'
- ;
-
+   FROM mgi_note_vocevidence_view nvv
+;
