c484f13c196178de4d5b9a594d02233dc453814b
TomConlin <corporate.drone1339@gmail.com>
2020-05-04 13:48:55 -0700

    rewrite

--- resources/mgi/SQL/monarch_gxd_allelepair_view.sql
+++ resources/mgi/SQL/monarch_gxd_allelepair_view.sql
@@ -1,52 +1,39 @@
 /*
 First derived from View "mgd.gxd_allelepair_view"
 
- ~ 5 seconds
+ ~ 1 minute
 
 */
- SELECT a._allelepair_key,
-    a._genotype_key,
-    a._allele_key_1,
-    a._allele_key_2,
-    a._marker_key,
-    a._mutantcellline_key_1,
-    a._mutantcellline_key_2,
-    a._pairstate_key,
-    a._compound_key,
-    a.sequencenum,
-    m.symbol,
-    m.chromosome,
-    a1.symbol AS allele1,
-    a2.symbol AS allele2,
-    t1.term AS allelestate,
-    t2.term AS compound
-   FROM gxd_allelepair a
-     JOIN mrk_marker m ON a._marker_key = m._marker_key
-     JOIN all_allele a1 ON a._allele_key_1 = a1._allele_key
-     JOIN all_allele a2 ON a._allele_key_2 = a2._allele_key
-     JOIN voc_term t1 ON a._pairstate_key = t1._term_key
-     JOIN voc_term t2 ON a._compound_key = t2._term_key
-UNION
- SELECT a._allelepair_key,
-    a._genotype_key,
-    a._allele_key_1,
-    a._allele_key_2,
-    a._marker_key,
-    a._mutantcellline_key_1,
-    a._mutantcellline_key_2,
-    a._pairstate_key,
-    a._compound_key,
-    a.sequencenum,
-    m.symbol,
-    m.chromosome,
-    a1.symbol AS allele1,
-    NULL::text AS allele2,
-    t1.term AS allelestate,
-    t2.term AS compound
-   FROM gxd_allelepair a
-     JOIN mrk_marker m ON a._marker_key = m._marker_key
-     JOIN all_allele a1 ON a._allele_key_1 = a1._allele_key
-     JOIN voc_term t1 ON a._pairstate_key = t1._term_key
-     JOIN voc_term t2 ON a._compound_key = t2._term_key
-  WHERE a._allele_key_2 IS NULL
+
+SELECT
+	gav._allelepair_key,
+    --gav._genotype_key,
+    ggv.strain,
+    ggv.mgiid,
+    --gav._allele_key_1,
+    --gav._allele_key_2,
+    --gav._marker_key,
+    --gav._mutantcellline_key_1,
+    cl1.cellline as mutantcellline1,
+    --gav._mutantcellline_key_2,
+    cl2.cellline as mutantcellline2,
+    --gav._pairstate_key,
+    --gav._compound_key,
+    gav.sequencenum,
+    --a._createdby_key,
+    --a._modifiedby_key,
+    --a.creation_date,
+    --a.modification_date,
+
+    gav.symbol,
+    gav.chromosome,
+    gav.allele1,
+    gav.allele2,
+    gav.allelestate,
+    gav.compound
+
+FROM gxd_allelepair_view gav
+join gxd_genotype_view ggv on gav._genotype_key = ggv._genotype_key
+left join all_cellline_view cl1 on gav._mutantcellline_key_1 = cl1._cellline_key
+left join all_cellline_view cl2 on gav._mutantcellline_key_2 = cl2._cellline_key
 ;
