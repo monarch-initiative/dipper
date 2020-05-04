717af470df5e3f331831e868ce254570e93d3202
TomConlin <corporate.drone1339@gmail.com>
2020-05-04 15:27:15 -0700

    rewrite

--- resources/mgi/SQL/monarch_prb_strain_acc_view.sql
+++ resources/mgi/SQL/monarch_prb_strain_acc_view.sql
@@ -1,16 +1,28 @@
 /*
 First derived from View "mgd.prb_strain_acc_view":
-~  todo broken
+~  5 seconds
 */
- SELECT a._accession_key,
-    a.accid,
-    mt.name as mgi_type,
-    a._object_key as mgi_internal,
-    al.name AS logical_name
-   FROM acc_accession a
-    join acc_logicaldb al on a._logicaldb_key = al._logicaldb_key
-    join mrk_types mt on a._mgitype_key = mt._marker_type_key
-  WHERE  mt.name = 'Complex/Cluster/Region' 
-    AND a.private != 1
-    AND a.preferred = 1
-  ;
+
+
+SELECT
+	psav._accession_key,
+    psav.accid,
+    psav.prefixpart,
+    psav.numericpart,
+    --a._logicaldb_key,
+    psav._object_key as mgi_internal,
+    --a._mgitype_key,
+    at.name as mgitype,
+    --a.private,
+    --a.preferred,
+    --a._createdby_key,
+    --a._modifiedby_key,
+    --a.creation_date,
+    --a.modification_date,
+    psav.logicaldb
+FROM prb_strain_acc_view  psav
+join acc_mgitype at on psav._mgitype_key = at._mgitype_key
+
+WHERE psav.private != 1
+  AND psav.preferred = 1
+;
