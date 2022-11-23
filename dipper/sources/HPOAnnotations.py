7fca275e1982663808ca206eefe6d3679a325f51
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 02:39:13 +0000

    Adding tarfile member sanitization to extractall()

--- dipper/sources/HPOAnnotations.py
+++ dipper/sources/HPOAnnotations.py
@@ -357,7 +357,26 @@ class HPOAnnotations(Source):
         os.mkdir(repo_dir)
 
         with tarfile.open('raw/hpoa/hpoa.tgz', 'r') as tarball:
-            tarball.extractall(repo_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tarball, repo_dir)
 
         # TO-DO add this to the dataset object
         # hmm ...kay... have git commit-hash in tarball repo name
